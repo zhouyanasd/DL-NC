5d61bad69bf553d5bb17ee41d05ffc88b43a4e1b
scarecrow <zhouyanasd@gmail.com>
2016-10-07 09:21:20 +0800

    10.7 home example

--- example.py
+++ example.py
@@ -19,4 +19,13 @@ import theano
 # print(f(np.arange(12).reshape((3,4)),10*np.ones((3,4))))
 
 class Layer(object):
-    def __init__(self):
+    def __init__(self, inputs, in_size, out_size, activation_function = None):
+        self.W = theano.shared(np.random.normal(0,1,(in_size,out_size)))
+        self.b = theano.shared(np.zeros((out_size,))+0.1)
+        self.Wx_plus_b = T.dot(inputs,self.W) + self.b
+        self.activation_function = activation_function
+        if activation_function is None:
+            self.outputs = self.Wx_plus_b
+        else:
+            self.outputs = self.activation_function(self.Wx_plus_b)
+
