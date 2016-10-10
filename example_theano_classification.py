9ff225d16ed6a05519fb4d2371c03a99d43f1027
scarecrow <zhouyanasd@gmail.com>
2016-10-10 09:49:28 +0800

    first example

--- example_theano_classification.py
+++ example_theano_classification.py
@@ -21,13 +21,33 @@ D = (rng.randn(N, feats), rng.randint(size=N, low=0, high=2))
 x = T.dmatrix('x')
 y = T.dvector('y')
 
-#initialize the weights and biases
-W = theano.shared(rng.randn(feats), name= 'w')
-b = theano.shared(0.1,name='b')
+# initialize the weights and biases
+W = theano.shared(rng.randn(feats), name='w')
+b = theano.shared(0.1, name='b')
 
-#Construct Theano expression graph
-p_1 = T.nnet.sigmoid(T.dot(x,W)+b)
-prediction = p_1>0.5
-xent = -y*T.log(p_1)-(1-y)*T.log(1-p_1)  #cost
+# Construct Theano expression graph
+p_1 = T.nnet.sigmoid(T.dot(x, W) + b)
+prediction = p_1 > 0.5
+xent = -y * T.log(p_1) - (1 - y) * T.log(1 - p_1)  # cost
 cost = xent.mean()
-gW,gb = T.grad(cost,[W,b])
+gW, gb = T.grad(cost, [W, b])
+
+# Compile
+learning_rate = 0.1
+train = theano.function(
+          inputs=[x, y],
+          outputs=[prediction, xent.mean()],
+          updates=((W, W - learning_rate * gW), (b, b - learning_rate * gb)))
+predict = theano.function(inputs=[x], outputs=prediction)
+
+# Training
+for i in range(500):
+    pred, err = train(D[0], D[1])
+    if i % 50 == 0:
+        print('cost:', err)
+        print("accuracy:", compute_accuracy(D[1], predict(D[0])))
+
+print("target values for D:")
+print(D[1])
+print("prediction on D:")
+print(predict(D[0]))
