4b3b08373eda0dba19c3d4b314933d5dbd15b57c
scarecrow <zhouyanasd@gmail.com>
2016-10-25 20:26:17 +0800

    first example

--- example/SciPy-odeint.py
+++ example/SciPy-odeint.py
@@ -19,39 +19,49 @@ def quadratic(u,t,tor,a,ur,uc):
     func = a*(x-ur)*(x-uc)
     return np.array([func/tor,y])
 
-def Izhikevich(w,t,ii,j):
-    a,b,c,d =0.02,0.2,-65,6
+def Izhikevich(w,t,ii,a=0.02,b=0.2,c=-65,d=6):
+    # a,b,c,d =0.02,0.2,-65,6
     # I = -500*(np.sin(t)+1)
     v,u =w
     # print("I:", I)
     # print("i:", ii(t))
     temp = np.array([0.04*(v**2)+5*v+140-u+ii(t),a*(b*v-u)])
-    if temp[0] >=30:
-        v=c
-        u=u+d
-        print(t,":",temp[0])
+    # if temp[0] >=30:
+    #     v=c
+    #     u=u+d
+    print(t,":",temp[1])
     return temp
 
 
-t = np.arange(0,100,0.1)
+
 def ii(t):
-    i = 10
+    i = 50
     return i
 
-def h_Izh():
-    W = theano.shared()
-    track3 = odeint(Izhikevich,(-75,-4),t,(ii,1))
-    print("hehe")
-    return track3
+def h_Izh(track=np.array([]).reshape(0,2),w0 = (-75,-4),time = 1,time_scale = 0.1):
+    w =w0
+    temp_track = track
+    for i in range(0,time,1):
+        t = np.arange(time_scale*i,time_scale*(i+2),time_scale)#i have to plus at lest 2 time slot
+        temp_track_t = odeint(Izhikevich,(w),t,(ii,0.02,0.2,-65,6))
+        temp_track = np.vstack((temp_track ,temp_track_t[1]))
+        if temp_track_t[1,0]<30:
+            w=(temp_track_t[1,0],temp_track_t[1,1])
+        else:
+            w = (-65,temp_track_t[1,1]+6)
+    return temp_track
 # track = odeint(Leaky_IandF,(0,0),t,(1,0.2,1))
 # track2 = odeint(quadratic,(-0.19,0),t,(1.5,1,-0.4,-0.2))
 # track3 = odeint(Izhikevich,(-16,-56),t,(ii,1))
 track3 = h_Izh()
+print(track3[:,0],track3.shape)
 
-fig = plt.figure()
-# plt.plot(t,track3)
-plt.plot(t[250:270],track3[250:270,0])
-# print(track2[0:22,0])
-fig2 = plt.figure()
-plt.plot(t[250:270],track3[250:270,1])
-plt.show()
+
+# t = np.arange(0,100,0.1)
+# fig = plt.figure()
+# # plt.plot(t,track3)
+# plt.plot(t[:],track3[:,0])
+# # print(track2[0:22,0])
+# fig2 = plt.figure()
+# plt.plot(t[:],track3[:,1])
+# plt.show()
--- example/spiking_neuro_network.py
+++ example/spiking_neuro_network.py
@@ -1,44 +1,68 @@
 from scipy.integrate import odeint
 import numpy as np
-
-class inter_act_func(object):
-    def __init__(self, t):
-        self.w=1
-        self.t = np.arange(0,100,100/t)
-        self.I = -500
+import matplotlib.pyplot as plt
 
 
-    def Leaky_IandF(self,u,t,tor,r,i):
-        x, y =u
-        sin = np.sin(50*t)*0.1+np.sin(30*t)+np.sin(100*t)+i
-        return np.array([(-x+r*i)/tor,(-y+r*sin)/tor])
-
-    def quadratic(self,u,t,tor,a,ur,uc):
-        x, y =u
-        func = a*(x-ur)*(x-uc)
-        return np.array([func/tor,y])
-
-    def Izhikevich(self,w,t):
-        a,b,c,d =0.02,0.2,-65,8
-        I = -500*(np.sin(t)+1)
-        u,v =w
-        return np.array([0.04*(v**2)+5*v+140-u+I,a*(b*v-u)])
+def ii(t):
+    i = 50
+    return i
 
 
+class InterActFunc(object):
+
+    def __init__(self, time_scale=0.1):
+        self.time_scale = time_scale
+
+
+    def __izhikevich(self,w, t, I, a, b):
+        v, u = w
+        temp = np.array([0.04 * (v ** 2) + 5 * v + 140 - u + I(t), a * (b * v - u)])
+        return temp
+
+
+    def izhikevich_spiking(self,I,w0 = (-75,-4),a=0.02,b=0.2,c=-65,d=6,
+                   time = 1000,track=np.array([]).reshape(0,2)):
+        w = w0
+        temp_track = track
+        for i in range(0,time,1):
+            t = np.arange(self.time_scale*i,self.time_scale*(i+2),self.time_scale)
+            temp_track_t = odeint(self.__izhikevich,(w),t,(I,a,b))
+            temp_track = np.vstack((temp_track ,temp_track_t[1]))
+            if temp_track_t[1,0]<30:
+                w = (temp_track_t[1,0],temp_track_t[1,1])
+            else:
+                w = (c,temp_track_t[1,1]+d)
+        return temp_track
 
 
 
-
-
-
-
-class spiking_neuron(object):
-    def __init__(self, input, activation_func=inter_act_func.Izhikevich):
+class SpikingNeuron(object):
+    def __init__(self, input, activation_func):
+        self.d_t =5  # time window
+        self.fired = False# is fired at last time slot
         self.W=1
         self.b=0.1
         self.t = 1
         self.output = 0
-        self.activate_func=activation_func
+        self.activation_func=activation_func
+
+    def input_trans(self):
+        pass
+
 
     def activate(self):
-        self.output = odeint(self.activation_func,self.t)
+        pass
+
+#---------------test---------------
+fun = InterActFunc()
+track = fun.izhikevich_spiking(I=ii)
+print(track[:,0],track.shape)
+
+
+t = np.arange(0,100,0.1)
+fig = plt.figure()
+plt.plot(t[:],track[:,0])
+fig2 = plt.figure()
+plt.plot(t[:],track[:,1])
+plt.show()
+#-----------------------------------
