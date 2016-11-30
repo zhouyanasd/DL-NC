f1e3c0d9ce57f34b5da0535fb10840c236a70048
scarecrow <zhouyanasd@gmail.com>
2016-11-30 13:19:40 +0800

    10.7 home example

--- example/spiking_neuro_network.py
+++ example/spiking_neuro_network.py
@@ -8,7 +8,10 @@ def ii(t):
     return i
 
 class Base(object):
-    __global_time = 0
+
+    def __init__(self):
+        self.__global_time = 0
+        self.__global_connection = 0
 
     def get_global_time(self):
         return self.get_global_time
@@ -16,7 +19,7 @@ class Base(object):
     def set_global_time(self,time):
         self.__global_time = time
 
-    def add_global_time(self,dt):
+    def add_global_time(self,dt = 1):
         self.__global_time = self.__global_time + dt
 
 
@@ -86,7 +89,14 @@ class SpikingNeuron(Base):
 
 
 class Synapse(Base):
-    pass
+
+    def __init__(self, pre_neuron, post_neuron, weight, delay):
+        self.pre_neuron = pre_neuron
+        self.post_neuron = post_neuron
+        self.weight = weight
+        self .delay = delay
+
+
 
 
 #---------------test---------------
