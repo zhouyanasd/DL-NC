44cd8a4a769c2bbad44b66f280b069356168b544
scarecrow <zhouyanasd@gmail.com>
2016-12-04 19:18:41 +0800

    10.7 home example

--- src/neuron/coding.py
+++ src/neuron/coding.py
@@ -4,15 +4,14 @@ from ..core import Base
 
 class Coding(Base):
 
-    def __init__(self,in_size,single_input):
-        self.time_window_buffer = np.zeros((in_size,d_t)) #tiem window buffer
+    def __init__(self,in_size):
+        self.time_window_buffer = np.zeros((in_size,d_t))                   #tiem window buffer
         self.in_size = in_size
-        self.__input = single_input()
 
-    def rate_window(self): #transmit the input to analog signal(private)
+    def rate_window(self,input_t):                                     #transmit the input to analog signal(private)
         #window slide
         self.time_window_buffer[:,0:d_t-1]=self.time_window_buffer[:,1:d_t]
-        self.time_window_buffer[:,d_t-1]=self.__input
+        self.time_window_buffer[:,d_t-1]=input_t
         #caculate the value
         l = np.sum(self.time_window_buffer,axis= 1)/d_t
         return l
--- src/neuron/neuron.py
+++ src/neuron/neuron.py
@@ -6,53 +6,44 @@ from ..function import ActFunction
 
 
 class SpikingNeuron(Base):
-    def __init__(self, in_size, activation_func, coding_rule):
-        self.id = 0
+    def __init__(self, id, in_size, activation_func, coding_rule, init = (-75,-4)):
+        self.id = id
 
-        self.fired = False                                  # is fired at last time slot
-        self.fired_sequence = np.array([])                  # neuron fired sequence for all the time slot
-        self.membrane_potential = np.array([]).reshape(0,2) # membrane potential sequence
-        self.membrane_potential_now = np.zeros((1,2))       # the membrane potential for now
-        self.I = np.array([])                               # the transformed input sequence
-        self.I_now = 0                                      # the transformed input for now
-        self.I_inject = 0                                   # the transformed inject input for now
-        self.activation_func = getattr(ActFunction(),activation_func)              # activation function for this neuron
-        self.coding_rule = coding_rule                      # coding rule for this neuron
+        self.fired = False                                                          # is fired at last time slot
+        self.fired_sequence = np.array([])                                          # neuron fired sequence for all the time slot
+        self.membrane_potential = np.array([]).reshape(0,2)                         # membrane potential sequence
+        self.membrane_potential_now = np.zeros((1,2))                               # the membrane potential for now
+        self.I = np.array([])                                                       # the transformed input sequence
+        self.I_now = 0                                                              # the transformed input for now
+        self.I_inject = 0                                                           # the transformed inject input for now
+        self.activation_func = getattr(ActFunction(),activation_func)               # activation function for this neuron
+        self.coding = getattr(Coding(in_size),coding_rule)                          # coding rule for this neuron
 
+        #TODO: the input size will be get from the synapses connected to this neuron
         self.in_size = in_size
-        self.b = 0.1
+
+        self.__init = init
+
+    def __trans_input(self,input_t):
+
+        #TODO: the input and weight will be calculate by synapse
+        W = np.abs(np.random.normal(0, 1, (1,self.in_size)))
+
+        l=self.coding(input_t)
+        self.I_now=(np.dot(W,l[:, np.newaxis])+self.b).reshape(1,)*20
+        self.I = np.hstack((self.I,self.I_now))
 
 
 
+    def activate(self,input_t,a=0.02,b=0.2,c=-65,d=6):
 
+        self.__trans_input(input_t)
 
-
-
-
-
-
-
-
-
-
-    def activate(self,input,init = (-75,-4),a=0.02,b=0.2,c=-65,d=6
-                   ,total_time_slot = 1000):
-
-         v = init
-         temp_track = np.array([]).reshape(0,2)
-         W = np.abs(np.random.normal(0, 1, (1,self.in_size)))
-
-
-
-         self.in_time_slot = self.in_time_slot +1
-         l=self.coding_rule.coding()
-         I=(np.dot(W,l[:, np.newaxis])+self.b).reshape(1,)*20
-         self.I = np.hstack((self.I,I))
-
-         temp_track_t= self.activation_func(self.I[self.in_time_slot],self.in_time_slot,v,a,b,c,d)
-         if temp_track_t[1,0]<30:
-            v = (temp_track_t[1,0],temp_track_t[1,1])
-         else:
-            v = (c,temp_track_t[1,1]+d)
-         temp_track = np.vstack((temp_track ,temp_track_t[1]))
-         return temp_track
+        self.membrane_potential_now= self.activation_func(self.I_now,self.__init,a,b,c,d)
+        if self.membrane_potential_now[1,0]<30:
+           self.fired = False
+           self.__init = (self.membrane_potential_now[1,0],self.membrane_potential_now[1,1])
+        else:
+           self.fired = True
+           self.__init = (c,self.membrane_potential_now[1,1]+d)
+        self.membrane_potential = np.vstack((self.membrane_potential ,self.membrane_potential_now[1]))
