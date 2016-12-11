31886ee92a2e87bcd88ca13b1d5f7a88bfc053ce
scarecrow <zhouyanasd@gmail.com>
2016-12-11 20:55:58 +0800

    finish the base of neuron and synapse and untested

--- src/input/input.py
+++ src/input/input.py
@@ -1,14 +1,15 @@
 import numpy as np
+import src
 
 from ..core import Base,INPUT_CONN_RATE
-from ..neuron import SpikingNeuron as Neuron
+
 
 class Input(Base):
     def __init__(self, input_size, reservoir):
         self.in_size = input_size
         self.input_t = np.zeros(self.in_size)
         self.reservoir = reservoir
-        self.conn_neuron = np.zeros((self.in_size, self.reservoir.r_size), dtype = np.dtype([('neuron', Neuron),('weight',np.float64)]))
+        self.conn_neuron = np.zeros((self.in_size, self.reservoir.r_size), dtype = np.dtype([('neuron', src.neuron.SpikingNeuron),('weight',np.float64)]))
 
     def __select_neuron_random(self):
         conn_n = np.random.randint(0,self.reservoir.r_size*INPUT_CONN_RATE,1)[0]                # get the connection number to reservoir for each input
