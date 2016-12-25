8ec0e3913ee8fca027af0fe02a61a48a1b944749
scarecrow <zhouyanasd@gmail.com>
2016-12-25 14:23:27 +0800

    10.7 home example

--- src/data/simple.py
+++ src/data/simple.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from..core import Base
+from..core import Base,TIME_SCALE
 
 class Simple(Base):
     def __init__(self, in_feature, in_number, group):
@@ -16,4 +16,31 @@ class Simple(Base):
             data.append(data_t)
         return data
 
+    def Tri_function(self):
+        rng = np.random
+        def chose_fun():
+            fun_type = np.arange(3)
+            rng.shuffle(fun_type)
+            fun = fun_type[:1]
+            return fun
+        def change_fun(rate):
+            fun_type = np.arange(100)
+            rng.shuffle(fun_type)
+            fun = fun_type[:1]
+            if fun > 100*rate:
+                return False
+            else:
+                return True
+        t = np.arange(self.in_number)
+        data = []
+        for i in (self.group):
+            for j in t:
+                if change_fun(0.05):
+
+
+                if chose_fun() == 0:
+                    sin = np.sin(t*TIME_SCALE)
+                    data_t = sin
+        data.append(data_t)
+        return data
 
