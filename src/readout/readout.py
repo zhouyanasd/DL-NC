74c81bdc669377b082e915100fe610a63c3e08a9
scarecrow <zhouyanasd@gmail.com>
2017-01-17 09:51:57 +0800

    first example

--- src/readout/readout.py
+++ src/readout/readout.py
@@ -1,7 +1,30 @@
 import numpy as np
+import src
 
 from ..core import Base
 
 class Readout(Base):
     def __init__(self):
+        self.pre_reservoir_list = []
+        self.read_number = 0
+
+    def add_pre_reservoir(self, reservoir):
+        self.pre_reservoir_list.append(reservoir)
+
+    def initialization(self):
+        pass
+
+    def add_read_neuron_s(self):
+        self.read_number += 1
+
+    def add_read_neuron_n(self, n_number):
+        self.read_number += n_number
+
+    def connect(self):
+        pass
+
+    def get_state(self):
+        pass
+
+    def output(self):
         pass
