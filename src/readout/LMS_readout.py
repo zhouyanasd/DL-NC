3da6dc79dad3219f6244fae90f870cc469aa762e
scarecrow <zhouyanasd@gmail.com>
2017-02-14 11:54:08 +0800

    lab

--- src/readout/LMS_readout.py
+++ src/readout/LMS_readout.py
@@ -2,10 +2,30 @@ import numpy as np
 
 from src.readout import Readout
 
-class LMS_readout(Readout ):
+class LMS_readout(Readout):
     def __int__(self, id):
         Readout.__init__(self, id)
+        self.LMS_num = 0
 
-    def LMS(self):
+    def add_LMS(self,class_num):
+        self.LMS_num += class_num
 
 
+
+    def LMS_train(self,all_state,):
+        pass
+
+    def LMS_test(self):
+        pass
+
+
+# class a():
+#     def aa(self):
+#         print('a')
+#
+# class b():
+#     def aa(self):
+#         print('b')
+#
+# B=b()
+# B.aa()
