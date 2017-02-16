a48740dc845174f13ca8567055e4d928b6a025ce
scarecrow <zhouyanasd@gmail.com>
2017-02-16 15:17:34 +0800

    10.7 home example

--- src/readout/LMS_readout.py
+++ src/readout/LMS_readout.py
@@ -11,17 +11,17 @@ class LMS_readout(Readout):
     def LMS_train(self, label):
         p = np.random.randn(self.read_number, self.pre_state.shape[0])
         for i in self.read_number:
-            Para = leastsq(self.__error, p[i], args=(label[i], self.pre_state))
+            Para = leastsq(self.__error, p[i], args = (label[i], self.pre_state))
             self.Para_list.append(Para[0])
 
-    def LMS_test(self, Data_test):
+    def LMS_test(self, Data):
         Output = np.zeros(self.read_number)
         for i in self.read_number:
-            Output[i] = self.Para_list[i].dot(Data_test)
+            Output[i] = self.Para_list[i].dot(Data)
         return Output
 
     def __error(self,p,y, args):
-        f = 1                      #bias
+        f = 2                      #bias
         for i in range (len(args)):
             f += p[i]*args[i]
         return f-y
