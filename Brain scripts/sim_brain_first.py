55dbea21c6fa43dc2e6dd93cb1ecd32fd128de5e
scarecrow <zhouyanasd@gmail.com>
2017-04-15 17:33:07 +0800

    lab

--- Brain scripts/sim_brain_first.py
+++ Brain scripts/sim_brain_first.py
@@ -1,17 +1,23 @@
 from brian2 import *
 start_scope()
 
-tau = 1*ms
+tau = 5*ms
 eqs = '''
 dv/dt = (1-v)/tau : 1
 '''
 
-G = NeuronGroup(1, eqs, method='linear')
-M = StateMonitor(G, 'v', record=0)
+G = NeuronGroup(1, eqs, threshold='v>0.8', reset='v = 0', refractory=15*ms, method='linear')
 
-run(30*ms)
+statemon = StateMonitor(G, 'v', record=0)
+spikemon = SpikeMonitor(G)
 
-plot(M.t/ms, M.v[0])
+run(50*ms)
+
+plot(statemon.t/ms, statemon.v[0])
+for t in spikemon.t:
+    axvline(t/ms, ls='--', c='r', lw=3)
+axhline(0.8, ls=':', c='g', lw=3)
 xlabel('Time (ms)')
 ylabel('v')
+print("Spike times: %s" % spikemon.t[:])
 show()
