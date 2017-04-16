c32f24129d9d2179cd3a331084e635207810a31f
scarecrow <zhouyanasd@gmail.com>
2017-04-16 10:58:09 +0800

    10.7 home example

--- Brain scripts/sim_brain_first.py
+++ Brain scripts/sim_brain_first.py
@@ -1,23 +1,36 @@
 from brian2 import *
 start_scope()
 
-tau = 5*ms
-eqs = '''
-dv/dt = (1-v)/tau : 1
-'''
+# tau = 5*ms
+# eqs = '''
+# dv/dt = (1-v)/tau : 1
+# '''
+#
+# G = NeuronGroup(1, eqs, threshold='v>0.8', reset='v = 0', refractory=15*ms, method='linear')
+#
+# statemon = StateMonitor(G, 'v', record=0)
+# spikemon = SpikeMonitor(G)
+#
+# run(50*ms)
+#
+# plot(statemon.t/ms, statemon.v[0])
+# for t in spikemon.t:
+#     axvline(t/ms, ls='--', c='r', lw=3)
+# axhline(0.8, ls=':', c='g', lw=3)
+# xlabel('Time (ms)')
+# ylabel('v')
+# print("Spike times: %s" % spikemon.t[:])
+# show()
 
-G = NeuronGroup(1, eqs, threshold='v>0.8', reset='v = 0', refractory=15*ms, method='linear')
+stimulus = TimedArray(np.hstack([[c, c, c, 0, 0]
+                                 for c in np.random.rand(1000)]),
+                                dt=10*ms)
+G = NeuronGroup(10, 'dv/dt = (-v + stimulus(t))/(10*ms) : 1',
+                threshold='v>1', reset='v=0')
+G.v = '0.5*rand()'  # different initial values for the neurons
 
 statemon = StateMonitor(G, 'v', record=0)
-spikemon = SpikeMonitor(G)
-
-run(50*ms)
 
 plot(statemon.t/ms, statemon.v[0])
-for t in spikemon.t:
-    axvline(t/ms, ls='--', c='r', lw=3)
-axhline(0.8, ls=':', c='g', lw=3)
-xlabel('Time (ms)')
-ylabel('v')
-print("Spike times: %s" % spikemon.t[:])
+
 show()
