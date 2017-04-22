b62f0e4794c401a49d03c5df18d274674f1b670c
scarecrow <zhouyanasd@gmail.com>
2017-04-22 12:55:15 +0800

    Revert "home"
    This reverts commit d245f60b5e00123e535f0bb81f9273084efeb724.

--- Brain scripts/sim_brain_four.py
+++ Brain scripts/sim_brain_four.py
@@ -5,16 +5,43 @@ stimulus = TimedArray(np.hstack([[c, c, c, 0, 0]
                                  for c in np.random.rand(1000)]),
                                 dt=10*ms)
 
-equ = '''
-dv/dt = (-v + stimulus(t)*mV)/(10*ms) : volt
+equ1 = '''
+dv/dt = (-v + stimulus(t)*mV)/(10*ms) : volt (unless refractory)
 '''
 
-G = NeuronGroup(1, equ,threshold='v > 0.9*mV', reset='v = 0*mV',
+equ2 = '''
+dv/dt = (-v + I)/(10*ms) : volt (unless refractory)
+I : volt
+'''
+
+equ3 ='''
+dv/dt = (-v + I)/(10*ms) : 1
+rates : Hz  # each neuron's input has a different rate
+size : 1  # and a different amplitude
+I = size*(sin(2*pi*rates*t)+1) : 1
+'''
+
+G1 = NeuronGroup(1, equ1,threshold='v > 0.9*mV', reset='v = 0*mV',
                     refractory=1*ms, method='linear')
 
-m1=StateMonitor(G,'v',record=0)
+G2 = NeuronGroup(1, equ2,threshold='v > 0.9*mV', reset='v = 0*mV',
+                    refractory=1*ms, method='linear')
 
-run(200*ms)
+G2.run_regularly('''change = int(rand() < 0.5)
+                   I = change*(rand()*2)*mV + (1-change)*I''',
+                dt=50*ms)
 
-plot(m1.t/ms,m1.v[0],'-b', label='Neuron 0')
+G3 = NeuronGroup(2, equ3,method='euler',threshold='v > 0.9', reset='v = 0',refractory=1*ms)
+G3.rates = '10*Hz + i*Hz'
+G3.size = '(100-i)/100. + 0.1'
+
+m1=StateMonitor(G3,'I',record=0)
+m2=StateMonitor(G3,'v',record=0)
+
+run(1000*ms)
+plt.figure(figsize = (20,4))
+subplot(121)
+plot(m1.t/ms,m1.I[0],'-b', label='Neuron 0')
+subplot(122)
+plot(m2.t/ms,m2.v[0],'-b', label='Neuron 0')
 show()
