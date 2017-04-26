f276acd12eaacc7384ec257b23005f50ea18fe97
scarecrow <zhouyanasd@gmail.com>
2017-04-26 20:53:40 +0800

    lab

--- Brain scripts/sim_brain_six.py
+++ Brain scripts/sim_brain_six.py
@@ -22,8 +22,21 @@ def lms_test(Data, p):
         f += p[i] * Data[i]
     return f
 
+def readout(M,Y):
+    print(M[0])
+    n = len(M)
+    Data=[]
+    for i in range(n):
+        x = M[i].smooth_rate(window='gaussian', width=time_window)/ Hz
+        Data.append(x)
+    p0 = [1]*n
+    p0.append(0.1)
+    para = lms_train(p0, Z, Data)
+    return Data,para
+
+
 #-----parameter setting-------
-n = 3
+n = 50
 time_window = 10*ms
 duration = 500 * ms
 
@@ -38,6 +51,7 @@ on_pre = '''
 h+=w
 g+=w
 '''
+
 #-----simulation setting-------
 P = PoissonGroup(1, 50 * Hz)
 G = NeuronGroup(n, equ, threshold='v > 0.20', reset='v = 0', method='linear', refractory=0 * ms)
@@ -51,7 +65,7 @@ S.connect(j='k for k in range(n)')
 # S3.connect()
 # S4.connect()
 #
-S.w = 'rand()'
+S.w = '0.1+j*0.02'
 # S2.w = 'rand()*2'
 # S3.w = '-rand()/5'
 # S4.w = 'rand()'
@@ -63,33 +77,20 @@ S.w = 'rand()'
 
 #------run----------------
 m1 = StateMonitor(G, ('v', 'I'), record=True)
-m2 = SpikeMonitor(G)
-m3 = PopulationRateMonitor(G[0:1])
-m4 = PopulationRateMonitor(G[1:2])
-m5 = PopulationRateMonitor(G[2:3])
+M = []
+for i in range(G._N):
+    locals()['M' + str(i)] = PopulationRateMonitor(G[(i):(i + 1)])
+    M.append(locals()['M' + str(i)])
 m6 = PopulationRateMonitor(P)
-# print(G.state(name='v'))
 
 run(duration)
-# print(m2.spike_trains()[0])
-# print(G.t)
-# print(m2.t/ms, m2.i)
-# print('num: ',m2.num_spikes)
-# print('count: ', m2.count)
-# print('spike_trains: ', m2.spike_trains()[0])
-# print('smooth_rate_P: ', rate)
 
-#----lms_readout----
-print(m3)
-x1 = m3.smooth_rate(window='gaussian', width=time_window)/ Hz
-x2 = m4.smooth_rate(window='gaussian', width=time_window)/ Hz
-x3 = m5.smooth_rate(window='gaussian', width=time_window)/ Hz
-Z = m6.smooth_rate(window='gaussian', width=time_window)/ Hz
-Data = [x1,x2,x3]
-p0=[1,1,1,0.1]
-k1,k2,k3,b = lms_train(p0,Z,Data)
-para = [k1,k2,k3,b]
-print('k1= ',k1,'k2= ', k2, 'k3 = ', k3,'b = ', b)
+#----lms_readout----# S2.connect(condition='i!=j')
+#
+Z = (m6.smooth_rate(window='gaussian', width=time_window)/ Hz)**2-2000
+
+Data, para = readout(M,Z)
+print(para)
 Z_t = lms_test(Data,para)
 
 #------vis----------------
@@ -114,38 +115,14 @@ plot(m1.t / ms, m1.I[2], label='I')
 
 fig2 = plt.figure(figsize=(20, 10))
 subplot(411)
-plot(m3.t / ms, x1,label='neuron1' )
+plot(M[0].t / ms, Data[0],label='neuron1' )
 subplot(412)
-plot(m4.t / ms, x2,label='neuron2' )
+plot(M[1].t / ms, Data[1],label='neuron2' )
 subplot(413)
-plot(m5.t / ms, x3,label='neuron3')
+plot(M[2].t / ms, Data[2],label='neuron3')
 subplot(414)
 plot(m6.t / ms, Z,'-b', label='Z')
 plot(m6.t / ms, Z_t,'--r', label='Z_t')
 xlabel('Time (ms)')
 ylabel('rate')
 show()
-
-
-
-
-# def get_state(G, M, width):
-#     spike_trains = M.spike_trains()
-#     T = int(G.t/us)
-#     n = len(G.i)
-#     dt = G.clock.dt
-#     rate_G_ = np.zeros((n, T))
-#     rate_G = np.zeros((n, T))
-#     for g in range(n):
-#         for spike in spike_trains[g]:
-#             print('fuck: ',spike /ms* 10)
-#             rate_G_[g][int(spike /ms* 10)] = (10000 / 3)
-#     for g in range(n):
-#         width_dt = int(np.round(2 * width / dt))
-#         window = np.exp(-np.arange(-width_dt,
-#                                    width_dt + 1) ** 2 *
-#                         1. / (2 * (width / dt) ** 2))
-#         rate_G[g] = Quantity(np.convolve(spike_trains[g],
-#                                          window * 1. / sum(window),
-#                                          mode='same'), dim=hertz.dim)
-#     return rate_G
