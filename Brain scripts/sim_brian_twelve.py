58ed71e5fd07e67babc2d8cb3237f908bd74c9ab
scarecrow <zhouyanasd@gmail.com>
2017-05-23 18:34:10 +0800

    lab

--- Brain scripts/sim_brian_twelve.py
+++ Brain scripts/sim_brian_twelve.py
@@ -58,6 +58,81 @@ def binary_classification(neu =1, interval_l=5,interval_s = ms):
     P = SpikeGeneratorGroup(neu, indices, times)
     return P , label
 
+
+def Tri_function(duration):
+    rng = np.random
+    TIME_SCALE = defaultclock.dt
+    in_number = duration/TIME_SCALE
+
+    def sin_fun(l, c, t):
+        return (np.sin(c * t * TIME_SCALE) + 1) / 2
+
+    def tent_map(l, c, t):
+        temp = l
+        if (temp < 0.5 and temp >= 0):
+            temp = (c / 50) * temp
+            return temp
+        elif (temp >= 0.5 and temp <= 1):
+            temp = (c / 50) * (1 - temp)
+            return temp
+        else:
+            return 0
+
+    def constant(l, c, t):
+        return c / 100
+
+    def chose_fun():
+        c = rng.randint(0, 3)
+        if c == 0:
+            return sin_fun, c
+        elif c == 1:
+            return tent_map, c
+        elif c == 2:
+            return constant, c
+
+    def change_fun(rate):
+        fun = rng.randint(1, 101)
+        if fun > 100 * rate:
+            return False
+        else:
+            return True
+
+    def trans_cls(cla):
+        cla_tran = []
+        for i in range(3):
+            cla_t = np.zeros(cla.size)
+            for j in range(cla.size):
+                if cla[j] == i:
+                    cla_t[j] = 1
+                else:
+                    cla_t[j] = -1
+            cla_tran.append(cla_t)
+        return cla_tran
+
+    data_t = np.zeros(in_number)
+    cla_t = np.zeros(in_number)
+    cons = rng.randint(1, 101)
+    fun, c = chose_fun()
+
+    for t in range(in_number):
+        if change_fun(0.05):
+            cons = rng.randint(1, 101)
+            fun, c = chose_fun()
+            try:
+                data_t[t] = fun(data_t[t - 1], cons, t)
+                cla_t[t] = c
+            except IndexError:
+                data_t[t] = fun(rng.randint(0, 101), cons, t)
+                cla_t[t] = c
+        else:
+            try:
+                data_t[t] = fun(data_t[t - 1], cons, t)
+                cla_t[t] = c
+            except IndexError:
+                data_t[t] = fun(rng.randint(0, 101), cons, t)
+                cla_t[t] = c
+    return data_t, cla_t
+
 def label_to_obj(label,obj):
     temp = []
     for a in label:
