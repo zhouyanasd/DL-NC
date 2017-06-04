3d02383d1e4da07dbd2dc0e089f5a4a830f04b21
scarecrow <zhouyanasd@gmail.com>
2017-06-04 16:58:19 +0800

    10.7 home example

--- Brain scripts/test/test3.py
+++ Brain scripts/test/test3.py
@@ -2,20 +2,41 @@ from brian2 import *
 start_scope()
 
 equ_1 = '''
-I = stimulus(t)*w : 1
-w  = weight: 1
+I = stimulus(t,i): 1
+'''
+
+equ_2 = '''
+I : 1
+d_n : 1
+'''
+
+model='''
+w : 1
+I_post = w * I_pre : 1 (summed)
+d = 1 : 1
 '''
 
 # Data =np.linspace(0,11,12)
 Data =np.linspace(0,11,12).reshape(3,4)
+print(Data)
 stimulus = TimedArray(Data,dt=1*ms)
-weight = 1
+print(stimulus(2*ms,2))
 
-G = NeuronGroup(1, equ_1)
-M = StateMonitor(G, 'I', record=True)
+Input = NeuronGroup(4, equ_1,events={'input_sin':'1>0'})
+G = NeuronGroup(2, equ_2)
+S = Synapses(Input, G, model, on_pre= 'd_n += d', on_event={'pre':'input_sin'})
 
-# G.w = '[1,1,1,1]'
+S.connect()
+S.w = '1'
+
+M = StateMonitor(G, ('I','d_n'), record=True)
 
 run(2*ms)
 
-print(M.I)
+print(M.I[:])
+print(S.I_pre[:])
+print(S.I_post[:])
+print(M.d_n)
+
+for name, var in G.variables.items():
+    print('%r : %s' % (name, var))
