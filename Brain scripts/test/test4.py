cc12c315f2d07306f2baf106cab10a39454703f9
scarecrow <zhouyanasd@gmail.com>
2017-07-01 17:30:13 +0800

    lab

--- Brain scripts/test/test4.py
+++ Brain scripts/test/test4.py
@@ -29,20 +29,38 @@ m1=StateMonitor(G,'v',record=9)
 M = StateMonitor(G, 'I', record=9)
 m2 = SpikeMonitor(P)
 
-run(300*ms)
+net = Network(collect())
+net.run(300*ms)
 print(S.w)
 store("test4","../Data/test4")
-stop()
 
-start_scope()
-P1 = PoissonGroup(10, np.arange(10)*Hz + 50*Hz)
+# start_scope()
 G1 = NeuronGroup(10, equ, threshold='v > 0.9', reset='v = 0', method='linear',refractory=1*ms )
-S1 = Synapses(P, G, 'w = 2 : 1',on_pre = on_pre, method='linear', delay = 1*ms)
-S1.connect(j='i')
+m_g1=StateMonitor(G,'v',record=9)
+net.add(G1)
+net.add(m_g1)
+print(collect())
 
-restore("test4","../Data/test4")
-run(100*ms)
-print(S1.w)
+net.run(10*ms)
+
+print(m_g1.t)
+
+net.remove(G)
+net.remove(S)
+net.remove(m1)
+net.remove(M)
+
+net.run(10*ms)
+
+print(m1.t)
+# P1 = PoissonGroup(10, np.arange(10)*Hz + 50*Hz)
+# G1 = NeuronGroup(10, equ, threshold='v > 0.9', reset='v = 0', method='linear',refractory=1*ms )
+# S1 = Synapses(P, G, 'w = 2 : 1',on_pre = on_pre, method='linear', delay = 1*ms)
+# S1.connect(j='i')
+
+# restore("test4","../Data/test4")
+# run(100*ms)
+# print(S1.w)
 
 fig1 = plt.figure(figsize=(10,4))
 subplot(121)
