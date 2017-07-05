c767749b422091f4b881abfe9a4a8e4fbe395839
scarecrow <zhouyanasd@gmail.com>
2017-07-05 19:23:04 +0800

    lab

--- Brain scripts/test/test_opencv.py
+++ Brain scripts/test/test_opencv.py
@@ -1,17 +1,36 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture("/Users/scarecrow/PycharmProjects/DL-NC/Brain scripts/Data/KHT/person25_boxing_d4_uncomp.avi")
-print(cap.isOpened())
+cap = cv2.VideoCapture("../Data/KHT/person25_boxing_d4_uncomp.avi")
 
-while(cap.isOpened()):
+while (cap.isOpened()):
     ret, frame = cap.read()
+    if ret == True:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(100) & 0xFF == ord('q'):
+        cv2.imshow('image', gray)
+        k = cv2.waitKey(20)
+        if (k & 0xff == ord('q')):
+            break
+    else:
         break
 
 cap.release()
 cv2.destroyAllWindows()
+
+# import numpy as np
+# import cv2
+# import matplotlib.pyplot as plt
+#
+# # 图6-1中的矩阵
+# img = np.array([
+#     [[255, 0, 0], [0, 255, 0], [0, 0, 255]],
+#     [[255, 255, 0], [255, 0, 255], [0, 255, 255]],
+#     [[255, 255, 255], [128, 128, 128], [0, 0, 0]],
+# ], dtype=np.uint8)
+#
+# # 用matplotlib存储
+# plt.imsave('img_pyplot.jpg', img)
+#
+# # 用OpenCV存储
+# cv2.imwrite('img_cv2.jpg', img)
