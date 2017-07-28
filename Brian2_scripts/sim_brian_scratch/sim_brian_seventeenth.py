e3c1f52cfa4e0cda29102ed4b146bb1d5d7dbaf9
scarecrow <zhouyanasd@gmail.com>
2017-07-28 21:49:48 +0800

    lab

--- Brian2_scripts/sim_brian_scratch/sim_brian_seventeenth.py
+++ Brian2_scripts/sim_brian_scratch/sim_brian_seventeenth.py
@@ -36,28 +36,40 @@ apost += Apost
 w = clip(w+apre, 0, wmax)
 '''
 
-P = PoissonGroup(2, rates=15*Hz)
+P = PoissonGroup(2, rates = np.array([15,35])*Hz)
 
-G = NeuronGroup(2, equ, threshold='v > 0.20', reset='v = 0', method='linear', refractory=0 * ms, name = 'neurongroup')
+G = NeuronGroup(5, equ, threshold='v > 0.20', reset='v = 0', method='linear', refractory=0 * ms, name = 'neurongroup')
 
 S = Synapses(P, G, model_STDP, on_pre=on_pre_STDP, on_post= on_post_STDP, method='linear', name = 'synapses')
 
-S.connect(i=0, j=1)
+# S.connect(i=0, j=1)
+# S.connect(i=1, j=1)
+S.connect()
 
-S.w = '0.1+j*'+str(1/2)
+S.w = '0.3+i*'+str(0.3)
 
 M = StateMonitor(S, ['w', 'apre', 'apost'], record=True)
+M_G = StateMonitor(G, ['v','I'], record = True)
 
 net = Network(collect())
 net.store('first')
-net.run(5000*ms)
+S.w = '0.3+i*'+str(0.3)
 
-figure(figsize=(20, 8))
+fig1 = figure(figsize=(20, 8))
 subplot(211)
 plot(M.t/ms, M.apre[0], label='apre')
 plot(M.t/ms, M.apost[0], label='apost')
 legend()
 subplot(212)
-plot(M.t/ms, M.w[0], label='w')
+plot(M.t/ms, M.w.T, label='w')
 legend(loc='best')
 xlabel('Time (ms)')
+
+fig2 = figure(figsize=(20, 8))
+subplot(211)
+plot(M_G.t/ms, M_G.v[1], label='v 1')
+legend()
+subplot(212)
+plot(M_G.t/ms, M_G.I[1], label='I 1')
+legend()
+show()
