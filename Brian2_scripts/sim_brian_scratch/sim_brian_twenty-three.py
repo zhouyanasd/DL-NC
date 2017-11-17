febc4cbf8b107ae1d9ed136d2ea333d03093b77d
scarecrow <zhouyanasd@gmail.com>
2017-11-17 08:14:54 +0800

    PEP8

--- Brian2_scripts/sim_brian_scratch/sim_brian_twenty-three.py
+++ Brian2_scripts/sim_brian_scratch/sim_brian_twenty-three.py
@@ -1,28 +1,31 @@
-#----------------------------------------
+# ----------------------------------------
 # LSM for Tri-funcion test
 # multiple pre-train STDP and the distribution is different for different patterns
 # simulation 6--analysis 4
-#----------------------------------------
+# ----------------------------------------
 
 from brian2 import *
 from brian2tools import *
 from scipy.optimize import leastsq
 import scipy as sp
 
-prefs.codegen.target = "numpy"  #it is faster than use default "cython"
+prefs.codegen.target = "numpy"  # it is faster than use default "cython"
 start_scope()
 np.random.seed(103)
 
-#------define function------------
-def lms_train(p0,Zi,Data):
+
+# ------define function------------
+def lms_train(p0, Zi, Data):
     def error(p, y, args):
         l = len(p)
         f = p[l - 1]
         for i in range(len(args)):
             f += p[i] * args[i]
         return f - y
-    Para = leastsq(error,p0,args=(Zi,Data))
+
+    Para = leastsq(error, p0, args=(Zi, Data))
     return Para[0]
+
 
 def lms_test(M, p):
     l = len(p)
@@ -31,35 +34,37 @@ def lms_test(M, p):
         f += p[i] * M[i]
     return f
 
-def readout(M,Z):
+
+def readout(M, Z):
     n = len(M)
-    Data=[]
+    Data = []
     for i in M:
         Data.append(i)
-    p0 = [1]*n
+    p0 = [1] * n
     p0.append(0.1)
     para = lms_train(p0, Z, Data)
-    return Data,para
+    return Data, para
+
 
 def mse(y_test, y):
     return sp.sqrt(sp.mean((y_test - y) ** 2))
 
 
-def Tri_function(duration,obj=0):
+def Tri_function(duration, obj=0):
     rng = np.random
     TIME_SCALE = defaultclock.dt
-    in_number = int(duration/TIME_SCALE)
+    in_number = int(duration / TIME_SCALE)
 
     def sin_fun(l, c, t):
-        return (np.sin(c * t * TIME_SCALE/us) + 1) / 2
+        return (np.sin(c * t * TIME_SCALE / us) + 1) / 2
 
     def tent_map(l, c, t):
         temp = l
         if (temp < 0.5 and temp > 0):
-            temp = (c / 101+1) * temp
+            temp = (c / 101 + 1) * temp
             return temp
         elif (temp >= 0.5 and temp < 1):
-            temp = (c / 101+1) * (1 - temp)
+            temp = (c / 101 + 1) * (1 - temp)
             return temp
         else:
             return 0.5
@@ -92,15 +97,15 @@ def Tri_function(duration,obj=0):
     fun, c = chose_fun()
 
     for t in range(in_number):
-        if change_fun(0.7) and t % 50 ==0:
+        if change_fun(0.7) and t % 50 == 0:
             cons = rng.randint(1, 101)
             fun, c = chose_fun()
             try:
-                data_t= fun(data[t - 1], cons, t)
+                data_t = fun(data[t - 1], cons, t)
                 data.append(data_t)
                 cla.append(c)
             except IndexError:
-                data_t = fun(rng.randint(1, 101)/100, cons, t)
+                data_t = fun(rng.randint(1, 101) / 100, cons, t)
                 data.append(data_t)
                 cla.append(c)
         else:
@@ -109,14 +114,15 @@ def Tri_function(duration,obj=0):
                 data.append(data_t)
                 cla.append(c)
             except IndexError:
-                data_t= fun(rng.randint(1, 101)/100, cons, t)
+                data_t = fun(rng.randint(1, 101) / 100, cons, t)
                 data.append(data_t)
                 cla.append(c)
     cla = np.asarray(cla)
     data = np.asarray(data)
     return data, cla
 
-def label_to_obj(label,obj):
+
+def label_to_obj(label, obj):
     temp = []
     for a in label:
         if a == obj:
@@ -125,30 +131,34 @@ def label_to_obj(label,obj):
             temp.append(0)
     return np.asarray(temp)
 
+
 def classification(thea, data):
     def normalization_min_max(arr):
         arr_n = arr
         for i in range(arr.size):
-            x = float(arr[i] - np.min(arr))/(np.max(arr)- np.min(arr))
+            x = float(arr[i] - np.min(arr)) / (np.max(arr) - np.min(arr))
             arr_n[i] = x
         return arr_n
+
     data_n = normalization_min_max(data)
     data_class = []
     for a in data_n:
-        if a >=thea:
+        if a >= thea:
             b = 1
         else:
             b = 0
         data_class.append(b)
-    return np.asarray(data_class),data_n
+    return np.asarray(data_class), data_n
 
-def ROC(y, scores, fig_title = 'ROC', pos_label=1):
+
+def ROC(y, scores, fig_title='ROC', pos_label=1):
     def normalization_min_max(arr):
         arr_n = arr
         for i in range(arr.size):
-            x = float(arr[i] - np.min(arr))/(np.max(arr) - np.min(arr))
+            x = float(arr[i] - np.min(arr)) / (np.max(arr) - np.min(arr))
             arr_n[i] = x
         return arr_n
+
     scores_n = normalization_min_max(scores)
     from sklearn import metrics
     fpr, tpr, thresholds = metrics.roc_curve(y, scores_n, pos_label=pos_label)
@@ -165,26 +175,27 @@ def ROC(y, scores, fig_title = 'ROC', pos_label=1):
     plt.ylabel('True Positive Rate')
     plt.title(fig_title)
     plt.legend(loc="lower right")
-    return fig, roc_auc , thresholds
+    return fig, roc_auc, thresholds
+
 
 ###############################################
-#-----parameter and model setting-------
+# -----parameter and model setting-------
 obj = 2
 n = 100
-pre_train_duration = 50*ms
+pre_train_duration = 50 * ms
 duration = 50 * ms
-duration_test = 50 *ms
+duration_test = 50 * ms
 pre_train_loop = 0
 interval_s = defaultclock.dt
 threshold = 0.3
 
-t0 = int(duration/ interval_s)
-t1 = int((duration+duration_test) / interval_s)
+t0 = int(duration / interval_s)
+t1 = int((duration + duration_test) / interval_s)
 
-taupre = taupost = 2*ms
+taupre = taupost = 2 * ms
 wmax = 1
 Apre = 0.005
-Apost = -Apre*taupre/taupost*1.2
+Apost = -Apre * taupre / taupost * 1.2
 
 equ = '''
 r : 1
@@ -214,7 +225,7 @@ h+=w
 g+=w
 '''
 
-model_STDP= '''
+model_STDP = '''
 w : 1
 dapre/dt = -apre/taupre : 1 (clock-driven)
 dapost/dt = -apost/taupost : 1 (clock-driven)
@@ -227,37 +238,38 @@ apre += Apre
 w = clip(w+apost, 0, wmax)
 '''
 
-on_post_STDP= '''
+on_post_STDP = '''
 apost += Apost
 w = clip(w+apre, 0, wmax)
 '''
 
-#-----simulation setting-------
-data , label = Tri_function(duration+duration_test)
-stimulus = TimedArray(data,dt=defaultclock.dt)
+# -----simulation setting-------
+data_pre, label_pre = Tri_function(pre_train_duration, obj=obj)
+data, label = Tri_function(duration + duration_test)
+stimulus = TimedArray(data, dt=defaultclock.dt)
 
-G = NeuronGroup(n, equ, threshold='v > 0.20', reset='v = 0', method='euler', refractory=1* ms,
-                name = 'neurongroup')
+G = NeuronGroup(n, equ, threshold='v > 0.20', reset='v = 0', method='euler', refractory=1 * ms,
+                name='neurongroup')
 
-G2 = NeuronGroup(int(n/4), equ, threshold ='v > 0.20', reset='v = 0', method='euler', refractory=1 * ms,
-                 name = 'neurongroup_1')
+G2 = NeuronGroup(int(n / 4), equ, threshold='v > 0.20', reset='v = 0', method='euler', refractory=1 * ms,
+                 name='neurongroup_1')
 
 G_lateral_inh = NeuronGroup(1, equ_h, threshold='v > 0.20', reset='v = 0', method='euler', refractory=1 * ms,
                             name='neurongroup_la_inh')
 
-G_readout = NeuronGroup(n,equ_1, method ='euler')
+G_readout = NeuronGroup(n, equ_1, method='euler')
 
-S2 = Synapses(G2, G, 'w : 1', on_pre = on_pre, method='linear', name = 'synapses_1')
+S2 = Synapses(G2, G, 'w : 1', on_pre=on_pre, method='linear', name='synapses_1')
 
 S5 = Synapses(G, G2, model_STDP, on_pre=on_pre_STDP, on_post=on_post_STDP, method='linear', name='synapses_4')
 
-S4 = Synapses(G, G, model_STDP, on_pre = on_pre_STDP, on_post = on_post_STDP, method = 'linear',  name = 'synapses_3')
+S4 = Synapses(G, G, model_STDP, on_pre=on_pre_STDP, on_post=on_post_STDP, method='linear', name='synapses_3')
 
 S6 = Synapses(G_lateral_inh, G, 'w : 1', on_pre=on_pre, method='linear', name='synapses_5')
 
 S_readout = Synapses(G, G_readout, 'w = 1 : 1', on_pre=on_pre, method='linear')
 
-#-------network topology----------
+# -------network topology----------
 S2.connect(p=1)
 S4.connect(p=0.5, condition='i != j')
 S5.connect(p=1)
@@ -278,28 +290,27 @@ S4.delay = '0*ms'
 G.r = '1'
 G2.r = '1'
 
-#------monitor----------------
+# ------monitor----------------
 m_w2 = StateMonitor(S4, 'w', record=True)
-m_g = StateMonitor(G, (['I','v']), record = True)
-m_g2 = StateMonitor(G2, (['I','v']), record = True)
-m_read = StateMonitor(G_readout, ('I'), record = True)
+m_g = StateMonitor(G, (['I', 'v']), record=True)
+m_g2 = StateMonitor(G2, (['I', 'v']), record=True)
+m_read = StateMonitor(G_readout, ('I'), record=True)
 
-#------create network-------------
+# ------create network-------------
 net = Network(collect())
 net.store('first')
 # fig0 =plt.figure(figsize=(4,4))
 # brian_plot(S4.w)
-print('S4.w = %s'%S4.w)
+print('S4.w = %s' % S4.w)
 ###############################################
-#------pre_train------------------
+# ------pre_train------------------
+stimulus.values = data_pre
 for loop in range(pre_train_loop):
-    data_pre, label_pre = Tri_function(pre_train_duration, obj=obj)
-    stimulus.values = data_pre
     net.run(pre_train_duration)
 
     # ------plot the weight----------------
     fig2 = plt.figure(figsize=(10, 4))
-    title('loop: '+str(loop))
+    title('loop: ' + str(loop))
     subplot(111)
     plot(m_w2.t / second, m_w2.w.T)
     xlabel('Time (s)')
@@ -309,7 +320,7 @@ for loop in range(pre_train_loop):
     net.restore('first')
     S4.w = net._stored_state['second']['synapses_3']['w'][0]
 
-#-------change the synapse model----------
+# -------change the synapse model----------
 stimulus.values = data
 
 S4.pre.code = '''
@@ -325,21 +336,21 @@ g+=w
 S5.post.code = ''
 
 ###############################################
-#------run for lms_train-------
+# ------run for lms_train-------
 net.store('third')
 net.run(duration)
 
-#------lms_train---------------
-y = label_to_obj(label[:t0],obj)
-Data,para = readout(m_read.I,y)
+# ------lms_train---------------
+y = label_to_obj(label[:t0], obj)
+Data, para = readout(m_read.I, y)
 
 #####################################
-#----run for test--------
+# ----run for test--------
 net.restore('third')
-net.run(duration+duration_test)
+net.run(duration + duration_test)
 
-#-----lms_test-----------
-y = label_to_obj(label,obj)
+# -----lms_test-----------
+y = label_to_obj(label, obj)
 y_t = lms_test(m_read.I, para)
 
 y_t_class, data_n = classification(threshold, y_t)
@@ -348,11 +359,12 @@ print('ROC of train is %s for classification of %s' % (roc_auc_train, obj))
 fig_roc_test, roc_auc_test, thresholds_test = ROC(y[t0:], data_n[t0:], 'ROC for test of %s' % obj)
 print('ROC of test is %sfor classification of %s' % (roc_auc_test, obj))
 
-
-
 #####################################
-#------vis of
+# ------vis of
 fig0 = plt.figure(figsize=(20, 4))
+subplot(211)
+plot(data_pre, 'r')
+subplot(212)
 plot(data, 'r')
 
 fig1 = plt.figure(figsize=(20, 8))
@@ -361,7 +373,7 @@ plt.scatter(m_read.t / ms, y_t_class, s=2, color="red", marker='o', alpha=0.6)
 plt.scatter(m_read.t / ms, y, s=3, color="blue", marker='*', alpha=0.4)
 plt.scatter(m_read.t / ms, data_n, s=2, color="green")
 axhline(threshold, ls='--', c='r', lw=1)
-plt.title('Classification Condition of threshold = %s'%threshold)
+plt.title('Classification Condition of threshold = %s' % threshold)
 
 fig3 = plt.figure(figsize=(20, 8))
 subplot(211)
@@ -386,4 +398,3 @@ legend(labels=[('I_%s' % k) for k in range(n)], loc='upper right')
 # fig6 =plt.figure(figsize=(4,4))
 # brian_plot(S4.w)
 show()
-
