fe79e96407c7fa3fd82b416c47229f7180dc9f19
scarecrow <zhouyanasd@gmail.com>
2017-11-27 16:50:50 +0800

    STDP_wmin_wmax

---
+++ Brian2_scripts/sim_brian_scratch/sim_brian_result_STDP_Tri.py
@@ -0,0 +1,4 @@
+#----------------------------------------------
+# Statistics Results Using parallel computation
+# Results for Tri-function in LSM with STDP
+#----------------------------------------------
