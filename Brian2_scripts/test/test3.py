6dcca23d9d4eac48ce057eabcb407df3d033a5ff
scarecrow <zhouyanasd@gmail.com>
2017-12-01 23:41:38 +0800

    more test3 demo

--- Brian2_scripts/test/test3.py
+++ Brian2_scripts/test/test3.py
@@ -9,6 +9,7 @@ I = stimulus(t,i): 1
 equ_2 = '''
 I : 1
 d_n : 1
+d_t : 1
 '''
 
 model='''
@@ -17,20 +18,26 @@ I_post = w * I_pre : 1 (summed)
 d = 1 : 1
 '''
 
+on_pre = '''
+d_n = 0
+d_n += w * I_pre
+d_t += d
+'''
+
 # Data =np.linspace(0,11,12)
 Data =np.linspace(0,11,12).reshape(3,4)
 print(Data)
 stimulus = TimedArray(Data,dt=1*ms)
 print(stimulus(2*ms,2))
 
-Input = NeuronGroup(4, equ_1,events={'input_sin':'1>0'})
+Input = NeuronGroup(4, equ_1,events={'input_sin':'t<2*ms'})
 G = NeuronGroup(2, equ_2)
-S = Synapses(Input, G, model, on_pre= 'd_n += d', on_event={'pre':'input_sin'})
+S = Synapses(Input, G, model, on_pre= on_pre, on_event={'pre':'input_sin'})
 
 S.connect()
 S.w = '1'
 
-M = StateMonitor(G, ('I','d_n'), record=True)
+M = StateMonitor(G, ('I','d_n','d_t'), record=True)
 
 run(2*ms)
 
@@ -38,6 +45,7 @@ print(M.I[:])
 print(S.I_pre[:])
 print(S.I_post[:])
 print(M.d_n)
+print(M.d_t)
 
 for name, var in G.variables.items():
     print('%r : %s' % (name, var))
