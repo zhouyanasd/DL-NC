025350aa358c88ae71b76ff571772f9b31a931cf
scarecrow <zhouyanasd@gmail.com>
2017-12-05 23:12:41 +0800

    more test3 demo STA for Tri

--- Brian2_scripts/sim_brian_scratch/sim_brian_result_STDP_Tri.py
+++ Brian2_scripts/sim_brian_scratch/sim_brian_result_STDP_Tri.py
@@ -8,9 +8,11 @@ from brian2tools import *
 from scipy.optimize import leastsq
 import scipy as sp
 import pandas as pd
+from multiprocessing import Queue,Pool
 
 prefs.codegen.target = "numpy"  # it is faster than use default "cython"
-
+start_scope()
+np.random.seed(103)
 
 # ------define function------------
 def lms_train(p0, Zi, Data):
@@ -48,7 +50,7 @@ def mse(y_test, y):
     return sp.sqrt(sp.mean((y_test - y) ** 2))
 
 
-def Tri_function(duration, obj=0):
+def Tri_function(duration, obj=-1):
     rng = np.random
     TIME_SCALE = defaultclock.dt
     in_number = int(duration / TIME_SCALE)
@@ -71,7 +73,7 @@ def Tri_function(duration, obj=0):
         return c / 100
 
     def chose_fun():
-        if obj == 0:
+        if obj == -1:
             c = rng.randint(0, 3)
         else:
             c = obj
@@ -95,7 +97,7 @@ def Tri_function(duration, obj=0):
     fun, c = chose_fun()
 
     for t in range(in_number):
-        if change_fun(0.7) and t % 50 == 0:
+        if change_fun(0.7) and t % 100 == 0:
             cons = rng.randint(1, 101)
             fun, c = chose_fun()
             try:
@@ -161,225 +163,206 @@ def ROC(y, scores, fig_title='ROC', pos_label=1):
     from sklearn import metrics
     fpr, tpr, thresholds = metrics.roc_curve(y, scores_n, pos_label=pos_label)
     roc_auc = metrics.auc(fpr, tpr)
-    return roc_auc, thresholds
+
+    fig = plt.figure()
+    lw = 2
+    plt.plot(fpr, tpr, color='darkorange',
+             lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title(fig_title)
+    plt.legend(loc="lower right")
+    return fig, roc_auc, thresholds
 
 
 ###############################################
-loop = 5
-sta_data_tri = []
-sta_data_test = []
-for l in range(loop):
-    np.random.seed(l)
-    start_scope()
+# -----parameter and model setting-------
+obj = 1
+n = 20
+pre_train_duration = 200 * ms
+duration = 200 * ms
+duration_test = 200 * ms
+pre_train_loop = 0
+interval_s = defaultclock.dt
+threshold = 0.5
 
-    # -----parameter and model setting-------
-    obj = 1
-    n = 20
-    pre_train_duration = 500 * ms
-    duration = 500 * ms
-    duration_test = 500 * ms
-    pre_train_loop = 0
-    interval_s = defaultclock.dt
-    threshold = 0.3
+t0 = int(duration / interval_s)
+t1 = int((duration + duration_test) / interval_s)
 
-    t0 = int(duration / interval_s)
-    t1 = int((duration + duration_test) / interval_s)
+taupre = taupost = 0.5 * ms
+wmax = 0.5
+wmin = 0.1
+Apre = 0.003
+Apost = -Apre * taupre / taupost * 1.2
 
-    taupre = taupost = 2 * ms
-    wmax = 1
-    Apre = 0.005
-    Apost = -Apre * taupre / taupost * 1.2
+equ_in = '''
+I = stimulus(t) : 1
+'''
 
-    equ = '''
-    r : 1
-    dv/dt = (I-v) / (0.3*ms) : 1 (unless refractory)
-    dg/dt = (-g)/(0.15*ms*r) : 1
-    dh/dt = (-h)/(0.145*ms*r) : 1
-    I = tanh(g-h)*40 +I_0: 1
-    I_0 = stimulus(t)*w_g:1
-    w_g : 1
-    '''
+equ = '''
+r : 1
+I_0 : 1 
+dv/dt = (I-v) / (0.3*ms) : 1 (unless refractory)
+dg/dt = (-g)/(0.15*ms*r) : 1
+dh/dt = (-h)/(0.145*ms*r) : 1
+I = tanh(g-h)*20 +I_0: 1
+'''
 
-    equ_h = '''
-    r : 1
-    dv/dt = (I-v) / (0.3*ms) : 1 (unless refractory)
-    I = stimulus(t)*w_g:1
-    w_g : 1
-    '''
+equ_read = '''
+dg/dt = (-g)/(0.6*ms) : 1 
+dh/dt = (-h)/(0.58*ms) : 1
+I = tanh(g-h)*20 : 1
+'''
 
-    equ_1 = '''
-    dg/dt = (-g)/(0.9*ms) : 1 
-    dh/dt = (-h)/(0.87*ms) : 1
-    I = tanh(g-h)*20 : 1
-    '''
+model_STDP = '''
+w : 1
+dapre/dt = -apre/taupre : 1 (clock-driven)
+dapost/dt = -apost/taupost : 1 (clock-driven)
+'''
 
-    on_pre = '''
-    h+=w
-    g+=w
-    '''
+on_pre_input = '''
+I_0 = 0 
+I_0 += w * I_pre
+'''
 
-    model_STDP = '''
-    w : 1
-    dapre/dt = -apre/taupre : 1 (clock-driven)
-    dapost/dt = -apost/taupost : 1 (clock-driven)
-    '''
+on_pre = '''
+h+=w
+g+=w
+'''
 
-    on_pre_STDP = '''
-    h+=w
-    g+=w
-    apre += Apre
-    w = clip(w+apost, 0, wmax)
-    '''
+on_pre_STDP = '''
+h+=w
+g+=w
+apre += Apre
+w = clip(w+apost, wmin, wmax)
+'''
 
-    on_post_STDP = '''
-    apost += Apost
-    w = clip(w+apre, 0, wmax)
-    '''
+on_post_STDP = '''
+apost += Apost
+w = clip(w+apre, wmin, wmax)
+'''
 
-    # -----simulation setting-------
-    data_pre, label_pre = Tri_function(pre_train_duration, obj=obj)
-    data, label = Tri_function(duration + duration_test)
-    stimulus = TimedArray(data, dt=defaultclock.dt)
+# -----simulation setting-------
+data_pre, label_pre = Tri_function(pre_train_duration, obj=obj)
+data, label = Tri_function(duration + duration_test)
+stimulus = TimedArray(data, dt=defaultclock.dt)
 
-    G = NeuronGroup(n, equ, threshold='v > 0.20', reset='v = 0', method='euler', refractory=1 * ms,
-                    name='neurongroup')
+Input = NeuronGroup(1, equ_in, method='linear', events={'input':'True'}, name = 'neurongroup_input')
 
-    G2 = NeuronGroup(int(n / 4), equ, threshold='v > 0.20', reset='v = 0', method='euler', refractory=1 * ms,
-                     name='neurongroup_1')
+G = NeuronGroup(n, equ, threshold='v > 0.20', reset='v = 0', method='euler', refractory=0.1 * ms,
+                name='neurongroup')
 
-    G_lateral_inh = NeuronGroup(1, equ_h, threshold='v > 0.20', reset='v = 0', method='euler', refractory=1 * ms,
-                                name='neurongroup_la_inh')
+G2 = NeuronGroup(int(n / 4), equ, threshold='v > 0.20', reset='v = 0', method='euler', refractory=0.1 * ms,
+                 name='neurongroup_1')
 
-    G_readout = NeuronGroup(n, equ_1, method='euler')
+G_lateral_inh = NeuronGroup(1, equ, threshold='v > 0.20', reset='v = 0', method='euler', refractory=0.1 * ms,
+                            name='neurongroup_la_inh')
 
-    S2 = Synapses(G2, G, 'w : 1', on_pre=on_pre, method='linear', name='synapses_1')
+G_readout = NeuronGroup(n, equ_read, method='euler', name='neurongroup_read')
 
-    S5 = Synapses(G, G2, model_STDP, on_pre=on_pre_STDP, on_post=on_post_STDP, method='linear', name='synapses_4')
+S = Synapses(Input, G, 'w : 1', on_pre = on_pre_input ,method='linear', on_event={'pre':'input'}, name='synapses')
 
-    S4 = Synapses(G, G, model_STDP, on_pre=on_pre_STDP, on_post=on_post_STDP, method='linear', name='synapses_3')
+S2 = Synapses(G2, G, 'w : 1', on_pre=on_pre, method='linear', name='synapses_1')
 
-    S6 = Synapses(G_lateral_inh, G, 'w : 1', on_pre=on_pre, method='linear', name='synapses_5')
+S3 = Synapses(Input, G_lateral_inh, 'w : 1', on_pre = on_pre_input ,method='linear', on_event={'pre':'input'},
+              name='synapses_2')
 
-    S_readout = Synapses(G, G_readout, 'w = 1 : 1', on_pre=on_pre, method='linear')
+S5 = Synapses(G, G2, model_STDP, on_pre=on_pre_STDP, on_post=on_post_STDP, method='linear', name='synapses_4')
 
-    # -------network topology----------
-    S2.connect(p=0.5)
-    S4.connect(p=(2 / n), condition='i != j')
-    S5.connect(p=(4 / n))
-    S6.connect()
-    S_readout.connect(j='i')
+S4 = Synapses(G, G, model_STDP, on_pre=on_pre_STDP, on_post=on_post_STDP, method='linear', name='synapses_3')
 
-    G.w_g = 'rand()'
-    G2.w_g = '0'
-    G_lateral_inh.w_g = '0.5'
+S6 = Synapses(G_lateral_inh, G, 'w : 1', on_pre=on_pre, method='linear', name='synapses_5')
 
-    S2.w = '-rand()'
-    S4.w = 'rand()'
-    S5.w = 'rand()'
-    S6.w = '-rand()'
+S_readout = Synapses(G, G_readout, 'w = 1 : 1', on_pre=on_pre, method='linear')
 
-    S4.delay = '0*ms'
+# -------network topology----------
+S.connect(j='k for k in range(int(n*1))')
+S2.connect(p=1)
+S3.connect()
+S4.connect(p=1, condition='i != j')
+S5.connect(p=1)
+S6.connect(j='k for k in range(int(n*1))')
+S_readout.connect(j='i')
 
-    G.r = '1'
-    G2.r = '1'
+S.w = '0.6+j*'+str(0.4/n)
+S2.w = '-1'
+S3.w = '1'
+S4.w = 'rand()'
+S5.w = 'rand()'
+S6.w = '-rand()'
 
-    # ------monitor----------------
-    m_w = StateMonitor(S5, 'w', record=True)
-    m_w2 = StateMonitor(S4, 'w', record=True)
-    m_g = StateMonitor(G, (['I', 'v']), record=True)
-    m_g2 = StateMonitor(G2, (['I', 'v']), record=True)
-    m_read = StateMonitor(G_readout, ('I'), record=True)
-    m_inh = StateMonitor(G_lateral_inh, ('v'), record=True)
+S.delay = '0.3*ms'
+S4.delay = '0.3*ms'
 
-    # ------create network-------------
-    net = Network(collect())
-    net.store('first')
-    auc_test = []
-    auc_train = []
+G.r = '1'
+G2.r = '1'
+G_lateral_inh.r = '1'
 
-    ###############################################
-    # ------pre_train------------------
-    for epochs in range(3):
-        obj = epochs
+# ------monitor----------------
+m_read = StateMonitor(G_readout, ('I'), record=True)
+
+# ------create network-------------
+net = Network(collect())
+net.store('first')
+fig_init_w =plt.figure(figsize=(4,4))
+brian_plot(S4.w)
+# print('S4.w = %s' % S4.w)
+###############################################
+# ------pre_train------------------
+for epochs in range(3):
+    obj = epochs
+    net.restore('first')
+
+    stimulus.values = data_pre
+    for loop in range(pre_train_loop):
+        net.run(pre_train_duration)
+        net.store('second')
         net.restore('first')
+        S4.w = net._stored_state['second']['synapses_3']['w'][0]
+        S5.w = net._stored_state['second']['synapses_4']['w'][0]
 
-        stimulus.values = data_pre
-        for loop in range(pre_train_loop):
-            net.run(pre_train_duration)
+# -------change the synapse model----------
+stimulus.values = data
 
-            # ------plot the weight----------------
-            fig2 = plt.figure(figsize=(10, 8))
-            title('loop: ' + str(loop))
-            subplot(211)
-            plot(m_w.t / second, m_w.w.T)
-            xlabel('Time (s)')
-            ylabel('Weight / gmax')
-            subplot(212)
-            plot(m_w2.t / second, m_w2.w.T)
-            xlabel('Time (s)')
-            ylabel('Weight / gmax')
+S4.pre.code = '''
+h+=w
+g+=w
+'''
+S4.post.code = ''
 
-            net.store('second')
-            net.restore('first')
-            S4.w = net._stored_state['second']['synapses_3']['w'][0]
-            S5.w = net._stored_state['second']['synapses_4']['w'][0]
+S5.pre.code = '''
+h+=w
+g+=w
+'''
+S5.post.code = ''
 
-        # -------change the synapse model----------
-        stimulus.values = data
+###############################################
+# ------run for lms_train-------
+net.store('third')
+net.run(duration)
 
-        S4.pre.code = '''
-        h+=w
-        g+=w
-        '''
-        S4.post.code = ''
+# ------lms_train---------------
+y = label_to_obj(label[:t0], obj)
+Data, para = readout(m_read.I, y)
 
-        S5.pre.code = '''
-        h+=w
-        g+=w
-        '''
-        S5.post.code = ''
+#####################################
+# ----run for test--------
+net.restore('third')
+net.run(duration + duration_test)
 
-        ###############################################
-        # ------run for lms_train-------
-        net.store('third')
-        net.run(duration)
+# -----lms_test-----------
+y = label_to_obj(label, obj)
+y_t = lms_test(m_read.I, para)
 
-        # ------lms_train---------------
-        y = label_to_obj(label[:t0], obj)
-        Data, para = readout(m_read.I, y)
+y_t_class, data_n = classification(threshold, y_t)
+fig_roc_train, roc_auc_train, thresholds_train = ROC(y[:t0], data_n[:t0], 'ROC for train of %s' % obj)
+print('ROC of train is %s for classification of %s' % (roc_auc_train, obj))
+fig_roc_test, roc_auc_test, thresholds_test = ROC(y[t0:], data_n[t0:], 'ROC for test of %s' % obj)
+print('ROC of test is %s for classification of %s' % (roc_auc_test, obj))
 
-        #####################################
-        # ----run for test--------
-        net.restore('third')
-        net.run(duration + duration_test)
-
-        # -----lms_test-----------
-        y = label_to_obj(label, obj)
-        y_t = lms_test(m_read.I, para)
-
-        #####################################
-        #  ------calculate results----
-        y_t_class, data_n = classification(threshold, y_t)
-        roc_auc_train, thresholds_train = ROC(y[:t0], data_n[:t0], 'ROC for train of %s' % obj)
-        print('ROC of train is %s for classification of %s' % (roc_auc_train, obj))
-        roc_auc_test, thresholds_test = ROC(y[t0:], data_n[t0:], 'ROC for test of %s' % obj)
-        print('ROC of test is %s for classification of %s' % (roc_auc_test, obj))
-
-        auc_train.append(roc_auc_train)
-        auc_test.append(roc_auc_test)
-    sta_data_tri.append(auc_train)
-    sta_data_test.append(auc_test)
-
-    #####################################
-    # ------vis of results----
-fig_tri = plt.figure(figsize=(4, 4))
-df = pd.DataFrame(np.asarray(sta_data_tri),
-                  columns=['0', '1', '2'])
-df.boxplot()
-plt.title('Classification Condition of train')
-fig_test = plt.figure(figsize=(4, 4))
-df = pd.DataFrame(np.asarray(sta_data_test),
-                  columns=['0', '1', '2'])
-df.boxplot()
-plt.title('Classification Condition of test')
+#####################################
+# ------vis of results----
 show()
