aebbbe8a1b0b326df8e81e6863066c37d0d78a40
scarecrow <zhouyanasd@gmail.com>
2018-01-05 11:06:41 +0800

    adjust for ST-Coding Tri-function

---
+++ Brian2_scripts/sim_brian_scratch/sim_brian_tewnty_six.py
@@ -0,0 +1,6 @@
+# ----------------------------------------
+# LSM with STDP for JV test
+# add Input layer as input and the encoding is transformed into spike trains
+# simulation 7--analysis 3
+# ----------------------------------------
+
