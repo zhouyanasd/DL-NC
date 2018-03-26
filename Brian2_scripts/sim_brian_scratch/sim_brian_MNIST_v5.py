002c4243fa8dc921746e29feba8c7da2c66a5045
scarecrow <zhouyanasd@gmail.com>
2018-03-26 12:30:21 +0800

    784*1 MNIST test

--- Brian2_scripts/sim_brian_scratch/sim_brian_MNIST_v5.py
+++ Brian2_scripts/sim_brian_scratch/sim_brian_MNIST_v5.py
@@ -1,8 +1,9 @@
 # ----------------------------------------
-# LSM with STDP for MNIST test
+# LSM without STDP for MNIST test
 # add neurons to readout layer for multi-classification(one-versus-the-rest)
 # using softmax(logistic regression)
 # input layer is changed to 781*1 with encoding method
+# change the LSM structure according to Maass paper
 # ----------------------------------------
 
 from brian2 import *
@@ -173,29 +174,31 @@ def get_series_data(n, data_frame, duration, is_order=True, *args, **kwargs):
     label = data_frame_obj['label'][:n]
     return data_frame_s, label
 
-
 # -----parameter setting-------
-obj = 1
 duration = 100
 N_train = 100
 N_test = 100
-N_pre_train =100
 Dt = defaultclock.dt
-n = 1000
 pre_train_loop = 0
 sample = 2
-I_gain = 5
+
+n = 108
+i_EE = 30
+i_EI = 60
+i_IE = -19
+i_II = -19
+i_inE = 18
+i_inI = 9
+
 
 df_train = load_Data_MNIST(60000, '../../Data/MNIST_data/train-images.idx3-ubyte',
                                '../../Data/MNIST_data/train-labels.idx1-ubyte')
 df_test = load_Data_MNIST(10000, '../../Data/MNIST_data/t10k-images.idx3-ubyte',
                                '../../Data/MNIST_data/t10k-labels.idx1-ubyte')
 
-data_pre_train_s, label_pre_train = get_series_data(N_pre_train, df_train, duration, False, obj=[obj])
 data_train_s, label_train = get_series_data(N_train, df_train, duration, False)
 data_test_s, label_test = get_series_data(N_test, df_test, duration, False)
 
-duration_pre_train = len(data_pre_train_s) * Dt
 duration_train = len(data_train_s) * Dt
 duration_test = len(data_test_s) * Dt
 
@@ -204,29 +207,16 @@ I = stimulus(t,i) : 1
 '''
 
 equ = '''
-r : 1
 dv/dt = (I-v) / (3*ms) : 1 (unless refractory)
-dg/dt = (-g)/(1.5*ms*r) : 1
-dh/dt = (-h)/(1.45*ms*r) : 1
-I = tanh(g-h)*I_gain: 1
+dg/dt = (-g)/(0.3*ms) : 1
+dh/dt = (-h)/(0.25*ms) : 1
+I = (g-h)+13.5: 1
 '''
 
 equ_read = '''
-dg/dt = (-g)/(1.5*ms) : 1 
-dh/dt = (-h)/(1.45*ms) : 1
-I = tanh(g-h)*I_gain: 1
-'''
-
-model_STDP = '''
-w : 1
-wmax : 1
-wmin : 1
-Apre : 1
-Apost = -Apre * taupre / taupost * 1.2 : 1
-taupre : second
-taupost : second
-dapre/dt = -apre/taupre : 1 (clock-driven)
-dapost/dt = -apost/taupost : 1 (clock-driven)
+dg/dt = (-g)/(0.3*ms) : 1 
+dh/dt = (-h)/(0.25*ms) : 1
+I = (g-h): 1
 '''
 
 on_pre = '''
@@ -234,21 +224,8 @@ h+=w
 g+=w
 '''
 
-on_pre_STDP = '''
-h+=w
-g+=w
-apre += Apre
-w = clip(w+apost, wmin, wmax)
-'''
-
-on_post_STDP = '''
-apost += Apost
-w = clip(w+apre, wmin, wmax)
-'''
 
 # -----simulation setting-------
-Time_array_pre_train = TimedArray(data_pre_train_s, dt=Dt)
-
 Time_array_train = TimedArray(data_train_s, dt=Dt)
 
 Time_array_test = TimedArray(data_test_s, dt=Dt)
@@ -256,193 +233,79 @@ Time_array_test = TimedArray(data_test_s, dt=Dt)
 Input = NeuronGroup(784, equ_in, threshold='I > 0', reset='I = 0', method='linear', refractory=0 * ms,
                     name = 'neurongroup_input')
 
-G = NeuronGroup(n, equ, threshold='v > 0.20', reset='v = 0', method='euler', refractory=1 * ms,
+G_ex = NeuronGroup(n, equ, threshold='v > 15', reset='v = 13.5', method='euler', refractory=0.3 * ms,
                 name='neurongroup')
 
-G2 = NeuronGroup(int(n / 4), equ, threshold='v > 0.20', reset='v = 0', method='euler', refractory=1 * ms,
-                 name='neurongroup_1')
-
-G_lateral_inh = NeuronGroup(1, equ, threshold='v > 0.20', reset='v = 0', method='euler', refractory=1 * ms,
-                            name='neurongroup_la_inh')
+G_in = NeuronGroup(int(n/4), equ, threshold='v > 15', reset='v = 13.5', method='euler', refractory=0.2 * ms,
+                name='neurongroup')
 
 G_readout = NeuronGroup(n, equ_read, method='euler', name='neurongroup_read')
 
-S = Synapses(Input, G, 'w : 1', on_pre = on_pre ,method='linear', name='synapses')
+S_inE = Synapses(Input, G_ex, 'w : 1', on_pre = on_pre ,method='linear', name='synapses_inE')
 
-S2 = Synapses(G2, G, 'w : 1', on_pre=on_pre, method='linear', name='synapses_1')
+S_inI = Synapses(Input, G_in, 'w : 1', on_pre = on_pre ,method='linear', name='synapses_inI')
 
-S3 = Synapses(Input, G_lateral_inh, 'w : 1', on_pre = on_pre ,method='linear', name='synapses_2')
+S_EE = Synapses(G_ex, G_ex, 'w : 1', on_pre = on_pre ,method='linear', name='synapses_EE')
 
-S5 = Synapses(G, G2, model_STDP, on_pre=on_pre_STDP, on_post=on_post_STDP, method='linear', name='synapses_4')
+S_EI = Synapses(G_ex, G_in, 'w : 1', on_pre = on_pre ,method='linear', name='synapses_EI')
 
-S4 = Synapses(G, G, model_STDP, on_pre=on_pre_STDP, on_post=on_post_STDP, method='linear', name='synapses_3')
+S_IE = Synapses(G_in, G_ex, 'w : 1', on_pre = on_pre ,method='linear', name='synapses_IE')
 
-S6 = Synapses(G_lateral_inh, G, 'w : 1', on_pre=on_pre, method='linear', name='synapses_5')
+S_II = Synapses(G_in, G_in, 'w : 1', on_pre = on_pre ,method='linear', name='synapses_I')
 
-S_readout = Synapses(G, G_readout, 'w = 1 : 1', on_pre=on_pre, method='linear')
+S_E_readout = Synapses(G_ex, G_readout, 'w = 1 : 1', on_pre=on_pre, method='linear')
+
+S_I_readout = Synapses(G_ex, G_readout, 'w = 1 : 1', on_pre=on_pre, method='linear')
 
 # -------network topology----------
-S.connect(j='k for k in range(int(n))')
-S2.connect(p=0.2)
-S3.connect()
-S4.connect(p=0.1,condition='i != j')
-S5.connect(p=0.2)
-S6.connect(j='k for k in range(int(n))')
-S_readout.connect(j='i')
+S_inE.connect(p = 0.3)
+S_inI.connect(p = 0.3)
+S_EE.connect(p = 0.2)
+S_EI.connect(p = 0.2)
+S_IE.connect(p = 0.2)
+S_II.connect(p = 0.2)
+S_I_readout.connect(j='i')
+S_E_readout.connect(j='i')
 
-S4.wmax = '0.5+rand()*0.4'
-S5.wmax = '0.5+rand()*0.4'
-S4.wmin = '0.2+rand()*0.2'
-S5.wmin = '0.2+rand()*0.2'
-S4.Apre = S5.Apre = '0.01'
-S4.taupre = S4.taupost ='1*ms+rand()*4*ms'
-S5.taupre = S5.taupost ='1*ms+rand()*4*ms'
+G_ex.v = '13.5+1.5*rand()'
+G_in.v = '13.5+1.5*rand()'
 
-S.w = 'rand()'
-S2.w = '-0.2'
-S3.w = '0.8'
-S4.w = 'wmin+rand()*(wmax-wmin)'
-S5.w = 'wmin+rand()*(wmax-wmin)'
-S6.w = '-rand()'
+S_inE.w = 'i_inE*randn()+i_inE'
+S_inI.w = 'i_inI*randn()+i_inI'
+S_EE.w = 'i_EE*randn()+i_EE'
+S_IE.w = 'i_IE*randn()+i_IE'
+S_EI.w = 'i_EI*randn()+i_EI'
+S_II.w = 'i_II*randn()+i_II'
 
-S.delay = '3*ms'
-S4.delay = '3*ms'
-
-G.r = '1'
-G2.r = '1'
-G_lateral_inh.r = '1'
+S_EE.delay = 1.5
+S_EI.delay = 0.8
+S_IE.delay = 0.8
+S_II.delay = 0.8
 
 # ------monitor----------------
-m_w = StateMonitor(S5, 'w', record=True)
-m_w2 = StateMonitor(S4, 'w', record=True)
-m_g = StateMonitor(G, (['I', 'v']), record=True)
-m_g2 = StateMonitor(G2, (['I', 'v']), record=True)
+m_g_ex = StateMonitor(G_ex, (['I', 'v']), record=True)
+m_g_in = StateMonitor(G_in, (['I', 'v']), record=True)
 m_read = StateMonitor(G_readout, ('I'), record=True)
-m_inh = StateMonitor(G_lateral_inh, ('I','v'), record=True)
 m_in = StateMonitor(Input, ('I'), record=True)
-
 
 # ------create network-------------
 net = Network(collect())
 net.store('first')
-fig_init_w =plt.figure(figsize=(4,4))
-brian_plot(S4.w)
-
-
-###############################################
-# ------pre_train------------------
-stimulus = Time_array_pre_train
-for loop in range(pre_train_loop):
-    net.run(duration_pre_train, report='text')
-
-    # ------plot the weight----------------
-    fig2 = plt.figure(figsize=(10, 8))
-    title('loop: ' + str(loop))
-    subplot(211)
-    plot(m_w.t / second, m_w.w.T)
-    xlabel('Time (s)')
-    ylabel('Weight / gmax')
-    subplot(212)
-    plot(m_w2.t / second, m_w2.w.T)
-    xlabel('Time (s)')
-    ylabel('Weight / gmax')
-
-    net.store('second')
-    net.restore('first')
-    S4.w = net._stored_state['second']['synapses_3']['w'][0]
-    S5.w = net._stored_state['second']['synapses_4']['w'][0]
-
-# -------change the synapse model----------
-S5.pre.code = S4.pre.code = '''
-h+=w
-g+=w
-'''
-S5.post.code = S4.post.code = ''
-
-
-###############################################
-# ------run for lms_train-------
-del stimulus
-stimulus = Time_array_train
-net.store('third')
-net.run(duration_train, report='text')
-
-# ------lms_train---------------
-Y_train = one_versus_the_rest(label_train, selected=np.arange(10))
-states = get_states(m_read.I, duration*Dt , duration_train, sample)
-P = readout(states, Y_train)
-Y_train_ = lms_test(states,P)
-label_train_ = trans_max_to_label(Y_train_)
-score_train = accuracy_score(label_train, label_train_)
-
-#####################################
-# ----run for test--------
-del stimulus
-stimulus = Time_array_test
-net.restore('third')
-net.run(duration_test, report='text')
-
-# -----lms_test-----------
-Y_test = one_versus_the_rest(label_test, selected=np.arange(10))
-states = get_states(m_read.I, duration*Dt , duration_test, sample)
-Y_test_ = lms_test(states,P)
-label_test_ = trans_max_to_label(Y_test_)
-score_test = accuracy_score(label_test, label_test_)
-
-#####################################
-#----------show results-----------
-print('Train score: ',score_train)
-print('Test score: ',score_test)
-
-#####################################
-# ------vis of results----
-fig0 = plt.figure(figsize=(20, 6))
-subplot(311)
-plt.xlim((0, 500))
-plot(data_pre_train_s)
-subplot(312)
-plt.xlim((0, 500))
-plot(data_train_s)
-subplot(313)
-plt.xlim((0, 500))
-plot(data_test_s)
-
-fig3 = plt.figure(figsize=(20, 8))
-subplot(311)
-plt.xlim((0, 100))
-plt.plot(m_g.t / ms, m_g.v.T, label='v')
-legend(labels=[('V_%s' % k) for k in range(n)], loc='upper right')
-subplot(312)
-plt.xlim((0, 100))
-plt.plot(m_g.t / ms, m_g.I.T, label='I')
-legend(labels=[('I_%s' % k) for k in range(n)], loc='upper right')
-subplot(313)
-plt.xlim((0, 100))
-plt.plot(m_in.t / ms, m_in.I.T, label='I')
-legend(labels=[('I_%s' % k) for k in range(n)], loc='upper right')
-
-fig4 = plt.figure(figsize=(20, 8))
+fig_init_w =plt.figure(figsize=(4,16))
 subplot(411)
-plt.xlim((0, 100))
-plt.plot(m_g2.t / ms, m_g2.v.T, label='v')
-legend(labels=[('V_%s' % k) for k in range(n)], loc='upper right')
-subplot(412)
-plt.xlim((0, 100))
-plt.plot(m_g2.t / ms, m_g2.I.T, label='I')
-legend(labels=[('I_%s' % k) for k in range(n)], loc='upper right')
-subplot(413)
-plt.xlim((0, 100))
-plt.plot(m_inh.t / ms, m_inh.v.T, label='v')
-legend(labels=[('V_%s' % k) for k in range(n)], loc='upper right')
-subplot(414)
-plt.xlim((0, 100))
-plt.plot(m_inh.t / ms, m_inh.I.T, label='I')
-legend(labels=[('_%s' % k) for k in range(n)], loc='upper right')
+brian_plot(S_EE.w)
+subplot(421)
+brian_plot(S_EI.w)
+subplot(431)
+brian_plot(S_IE.w)
+subplot(441)
+brian_plot(S_II.w)
 
 
-fig5 = plt.figure(figsize=(20, 4))
-plt.plot(m_read.t / ms, m_read.I.T, label='I')
-legend(labels=[('I_%s' % k) for k in range(n)], loc='upper right')
 
-fig6 =plt.figure(figsize=(4,4))
-brian_plot(S4.w)
+
+
+
+
+
+
