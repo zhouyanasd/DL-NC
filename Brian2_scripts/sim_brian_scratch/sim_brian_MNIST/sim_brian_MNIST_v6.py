f0f6a45ae0fc59bc43de46beef713b361b401dd0
scarecrow <zhouyanasd@gmail.com>
2018-05-22 11:41:53 +0800

    MNIST_v6

--- Brian2_scripts/sim_brian_scratch/sim_brian_MNIST/sim_brian_MNIST_v6.py
+++ Brian2_scripts/sim_brian_scratch/sim_brian_MNIST/sim_brian_MNIST_v6.py
@@ -24,3 +24,279 @@ prefs.codegen.target = "numpy"
 start_scope()
 np.random.seed(100)
 data_path = '../../../Data/MNIST_data/'
+
+# ------define general function------------
+class Function():
+    def __init__(self):
+        pass
+
+    def logistic(self, f):
+        return 1/(1+np.exp(-f))
+
+    def softmax(self, z):
+        return np.array([(np.exp(i) / np.sum(np.exp(i))) for i in z])
+
+    def gamma(self, a, size):
+        return sp.stats.gamma.rvs(a, size = size)
+
+
+class Base():
+    def __init__(self, duration, dt):
+        self.duration = duration
+        self.dt = dt
+        self.interval = duration*dt
+
+    def get_states(self, input, running_time, sample, normalize = False):
+        n = int(running_time / self.interval)
+        step = int(self.interval / sample / defaultclock.dt)
+        interval_ = int(self.interval / defaultclock.dt)
+        temp = []
+        for i in range(n):
+            sum = np.sum(input[:, i * interval_: (i + 1) * interval_][:,::-step], axis=1)
+            temp.append(sum)
+        if normalize:
+            return MinMaxScaler().fit_transform(np.asarray(temp)).T
+        else:
+            return np.asarray(temp).T
+
+    def update_states(self, type = 'pandas', *args, **kwargs):
+        for seq, state in enumerate(kwargs):
+            if type == 'pandas':
+                kwargs[state] = kwargs[state].append(pd.DataFrame(args[seq]))
+            elif type == 'numpy':
+                kwargs[state] = self.np_extend(kwargs[state],args[seq],1)
+        return kwargs
+
+    def normalization_min_max(self, arr):
+        arr_n = arr
+        for i in range(arr.size):
+            x = float(arr[i] - np.min(arr)) / (np.max(arr) - np.min(arr))
+            arr_n[i] = x
+        return arr_n
+
+    def mse(self, y_test, y):
+        return sp.sqrt(sp.mean((y_test - y) ** 2))
+
+    def classification(self, thea, data):
+        data_n = self.normalization_min_max(data)
+        data_class = []
+        for a in data_n:
+            if a >= thea:
+                b = 1
+            else:
+                b = 0
+            data_class.append(b)
+        return np.asarray(data_class), data_n
+
+    def allocate(self, G, X, Y, Z):
+        V = np.zeros((X,Y,Z), [('x',float),('y',float),('z',float)])
+        V['x'], V['y'], V['z'] = np.meshgrid(np.linspace(0,X-1,X),np.linspace(0,X-1,X),np.linspace(0,Z-1,Z))
+        V = V.reshape(X*Y*Z)
+        np.random.shuffle(V)
+        n = 0
+        for g in G:
+            for i in range(g.N):
+                g.x[i], g.y[i], g.z[i]= V[n][0], V[n][1], V[n][2]
+                n +=1
+        return G
+
+
+    def w_norm2(self, n_post, Synapsis):
+        for i in range(n_post):
+            a = Synapsis.w[np.where(Synapsis._synaptic_post == i)[0]]
+            Synapsis.w[np.where(Synapsis._synaptic_post == i)[0]] = a/np.linalg.norm(a)
+
+    def np_extend(self, a, b, axis = 0):
+        if a == None:
+            shape = list(b.shape)
+            shape[axis] = 0
+            a = np.array([]).reshape(tuple(shape))
+        return np.append(a, b, axis)
+
+    def np_append(self, a, b):
+        shape = list(b.shape)
+        shape.insert(0, -1)
+        if a == None:
+            a = np.array([]).reshape(tuple(shape))
+        return np.append(a, b.reshape(tuple(shape)), axis= 0)
+
+
+class Readout():
+    def __init__(self, function):
+        self.function = function
+
+    def data_init(self, M_train, M_test, label_train, label_test, rate,theta):
+        self.rate = rate
+        self.theta = theta
+        self.iter = 0
+        self.X_train = self.add_bis(M_train)
+        self.X_test = self.add_bis(M_test)
+        self.Y_train = self.prepare_Y(label_train)
+        self.Y_test = self.prepare_Y(label_test)
+        self.P = np.random.rand(self.X_train.shape[1], self.Y_train.shape[1])
+        self.cost_train = 1e+100
+        self.cost_test = 1e+100
+
+    def predict_logistic(self,results):
+        labels = (results > 0.5).astype(int).T
+        return labels
+
+    def calculate_score(self,label, label_predict):
+        return [accuracy_score(i,j) for i,j in zip(label,label_predict)]
+
+    def add_bis(self,data):
+        one = np.ones((data.shape[1], 1))  # bis
+        X = np.hstack((data.T, one))
+        return X
+
+    def prepare_Y(self, label):
+        if np.asarray(label).ndim == 1:
+            return np.asarray([label]).T
+        else:
+            return np.asarray(label).T
+
+    def cost(self, X, Y, P):
+        left = np.multiply(Y, np.log(self.function(X.dot(P))))
+        right = np.multiply((1 - Y), np.log(1 - self.function(X.dot(P))))
+        return -np.sum(np.nan_to_num(left + right), axis=0) / (len(Y))
+
+    def train(self, X, Y, P):
+        P_ = P + X.T.dot(Y - self.function(X.dot(P))) * self.rate
+        return P_
+
+    def test(self, X, p):
+        return self.function(X.dot(p))
+
+    def stop_condition(self):
+        return ((self.cost_train - self.cost(self.X_train, self.Y_train, self.P))> self.theta).any() and \
+               ((self.cost_test - self.cost(self.X_test, self.Y_test, self.P)) > self.theta).any() or self.iter < 100
+
+    def readout(self):
+        self.iter = 0
+        while self.stop_condition():
+            self.iter += 1
+            self.cost_train = self.cost(self.X_train, self.Y_train, self.P)
+            self.cost_test = self.cost(self.X_test, self.Y_test, self.P)
+            self.P = self.train(self.X_train, self.Y_train, self.P)
+            if self.iter %10000 == 0:
+                print(self.iter, self.cost_train, self.cost_test)
+        print(self.iter, self.cost_train, self.cost_test)
+        return self.test(self.X_train, self.P), self.test(self.X_test, self.P)
+
+    def readout_sk(self, X_train, X_test, y_train, y_test):
+        from sklearn.linear_model import LogisticRegression
+        lr = LogisticRegression()
+        lr.fit(X_train.T, y_train.T)
+        y_train_predictions = lr.predict(X_train.T)
+        y_test_predictions = lr.predict(X_test.T)
+        return accuracy_score(y_train_predictions, y_train.T), accuracy_score(y_test_predictions, y_test.T)
+
+
+class Result():
+    def __init__(self):
+        pass
+
+    def result_save(self, path, *arg, **kwarg):
+        if os.path.exists(path):
+            os.remove(path)
+        fw = open(path, 'wb')
+        pickle.dump(kwarg, fw)
+        fw.close()
+
+    def result_pick(self, path):
+        fr = open(path, 'rb')
+        data = pickle.load(fr)
+        fr.close()
+        return data
+
+    def animation(self, t, v, interval, duration, a_step=10, a_interval=100, a_duration = 10):
+        xs = LinearScale()
+        ys = LinearScale()
+        line = Lines(x=t[:interval], y=v[:,:interval], scales={'x': xs, 'y': ys})
+        xax = Axis(scale=xs, label='x', grid_lines='solid')
+        yax = Axis(scale=ys, orientation='vertical', tick_format='0.2f', label='y', grid_lines='solid')
+        fig = Figure(marks=[line], axes=[xax, yax], animation_duration=a_duration)
+        def on_value_change(change):
+            line.x = t[change['new']:interval+change['new']]
+            line.y = v[:,change['new']:interval+change['new']]
+        play = widgets.Play(
+            interval=a_interval,
+            value=0,
+            min=0,
+            max=duration,
+            step=a_step,
+            description="Press play",
+            disabled=False
+        )
+        slider = widgets.IntSlider(min=0, max=duration)
+        widgets.jslink((play, 'value'), (slider, 'value'))
+        slider.observe(on_value_change, names='value')
+        return play, slider,
+
+
+class MNIST_classification():
+    def __init__(self, shape):
+        self.shape = shape
+    def load_Data_MNIST(self,n, path_value, path_label):
+        with open(path_value, 'rb') as f1:
+            buf1 = f1.read()
+        with open(path_label, 'rb') as f2:
+            buf2 = f2.read()
+
+        image_index = 0
+        image_index += struct.calcsize('>IIII')
+        im = []
+        for i in range(n):
+            temp = struct.unpack_from('>784B', buf1, image_index)
+            im.append(np.reshape(temp, (1, 784)))
+            image_index += struct.calcsize('>784B')
+
+        label_index = 0
+        label_index += struct.calcsize('>II')
+        label = np.asarray(struct.unpack_from('>' + str(n) + 'B', buf2, label_index))
+
+        f = lambda x: (x - np.min(x)) / (np.max(x) - np.min(x))
+        df = pd.DataFrame({'value': pd.Series(im).apply(f), 'label': pd.Series(label)})
+        return df
+
+    def encoding_latency_MNIST(self, analog_data, n, duration, min=0, max=np.pi, *args):
+        def encoding_cos(x, n, A):
+            encoding = []
+            for i in range(int(n)):
+                trans_cos = np.round(0.5 * A * (np.cos(x + np.pi * (i / n)) + 1)).clip(0, A - 1)
+                coding = [([0] * trans_cos.shape[1]) for i in range(A * trans_cos.shape[0])]
+                index_0 = 0
+                for p in trans_cos:
+                    index_1 = 0
+                    for q in p:
+                        coding[int(q)][index_1 + A * index_0] = 1
+                        index_1 += 1
+                    index_0 += 1
+                encoding.extend(coding)
+            return np.asarray(encoding)
+
+        f = lambda x: (max - min) * (x - np.min(x)) / (np.max(x) - np.min(x))
+        return analog_data.apply(f).apply(encoding_cos, n=n, A=duration)
+
+    def get_series_data(self, n, data_frame, duration, is_order=True, *args, **kwargs):
+        try:
+            obj = kwargs['obj']
+        except KeyError:
+            obj = np.arange(10)
+        if not is_order:
+            data_frame_obj = data_frame[data_frame['label'].isin(obj)].sample(frac=1).reset_index(drop=True)
+        else:
+            data_frame_obj = data_frame[data_frame['label'].isin(obj)]
+        data_frame_s = []
+        for value in self.encoding_latency_MNIST(data_frame_obj['value'][:n], kwargs['coding_n'], int(0.1 * duration)):
+            for data in value:
+                data_frame_s.append(list(data))
+            interval = duration - value.shape[0]
+            if interval > 0.3 * duration:
+                data_frame_s.extend([[0] * 784] * interval)
+            else:
+                raise Exception('duration is too short')
+        data_frame_s = np.asarray(data_frame_s)
+        label = data_frame_obj['label'][:n]
+        return data_frame_s, label
+
