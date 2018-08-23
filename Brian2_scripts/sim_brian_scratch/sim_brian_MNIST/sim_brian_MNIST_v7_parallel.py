29d63be3b63cf68827eca6c99189be30857a4bee
scarecrow <zhouyanasd@gmail.com>
2018-08-23 17:43:27 +0800

    MINIST parallel numpy a is None fixed

--- Brian2_scripts/sim_brian_scratch/sim_brian_MNIST/sim_brian_MNIST_v7_parallel.py
+++ Brian2_scripts/sim_brian_scratch/sim_brian_MNIST/sim_brian_MNIST_v7_parallel.py
@@ -113,7 +113,7 @@ class Base():
             Synapsis.w[np.where(Synapsis._synaptic_post == i)[0]] = a / np.linalg.norm(a)
 
     def np_extend(self, a, b, axis=0):
-        if a == None:
+        if a is None:
             shape = list(b.shape)
             shape[axis] = 0
             a = np.array([]).reshape(tuple(shape))
@@ -122,7 +122,7 @@ class Base():
     def np_append(self, a, b):
         shape = list(b.shape)
         shape.insert(0, -1)
-        if a == None:
+        if a is None:
             a = np.array([]).reshape(tuple(shape))
         return np.append(a, b.reshape(tuple(shape)), axis=0)
 
