f6d1708c70b0805435f79f9a73b2847b73af2b33
scarecrow <zhouyanasd@gmail.com>
2018-08-26 15:44:39 +0800

    weight_changed_mean and weight_changed
    GS for plasticity

--- Brian2_scripts/sim_brian_scratch/sim_brian_MNIST/sim_brian_MNIST_v7_STDP_v1.py
+++ Brian2_scripts/sim_brian_scratch/sim_brian_MNIST/sim_brian_MNIST_v7_STDP_v1.py
@@ -389,6 +389,7 @@ def run_net(inputs):
 def run_net_plasticity(inputs):
     spectral_radius = None
     weight_changed = None
+    weight_changed_mean = None
     monitor_record= {
         'm_g_ex.I': None,
         'm_g_ex.v': None,
@@ -406,14 +407,15 @@ def run_net_plasticity(inputs):
         stimulus = TimedArray(data, dt=Dt)
         net.run(duration*Dt)
         weight_trained = S_EE.variables['w'].get_value().copy()
-        weight_changed = base.np_append(weight_changed, np.mean(np.abs(weight_trained - weight_initial)))
+        weight_changed = base.np_append(weight_trained - weight_initial)
+        weight_changed_mean = base.np_append(weight_changed_mean, np.mean(np.abs(weight_trained - weight_initial)))
         if Switch_monitor:
             monitor_record = base.update_states('numpy', m_g_ex.I, m_g_ex.v, m_g_in.I, m_g_in.v, m_read.I,
                                                 m_read.v, m_input.I, m_s_ee.w, **monitor_record)
         net.restore('init')
         S_EE.w = weight_trained.copy()
     result.result_save('weight.pkl', {'weight' : weight_trained})
-    return weight_changed, spectral_radius, monitor_record
+    return weight_changed, weight_changed_mean, spectral_radius, monitor_record
 
 
 ###################################
@@ -618,7 +620,7 @@ net.store('init')
 ###############################################
 # ------run for plasticity-------
 if Switch_plasticity:
-    weight_changed, spectral_radius, monitor_record_pre_train = run_net_plasticity(data_plasticity_s)
+    weight_changed, weight_changed_mean, spectral_radius, monitor_record_pre_train = run_net_plasticity(data_plasticity_s)
 
 #------save monitor data and results------
 if Switch_monitor:
