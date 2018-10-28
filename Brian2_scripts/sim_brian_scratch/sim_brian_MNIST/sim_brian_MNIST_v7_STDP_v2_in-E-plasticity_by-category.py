bc5a3cc9f7f44ac3d5ba768e30b3ea0a70bf3509
scarecrow <zhouyanasd@gmail.com>
2018-10-28 22:56:17 +0800

    plasticity v2 in-E plasticity bug fixed

--- Brian2_scripts/sim_brian_scratch/sim_brian_MNIST/sim_brian_MNIST_v7_STDP_v2_in-E-plasticity_by-category.py
+++ Brian2_scripts/sim_brian_scratch/sim_brian_MNIST/sim_brian_MNIST_v7_STDP_v2_in-E-plasticity_by-category.py
@@ -518,8 +518,8 @@ def run_net_plasticity_by_category(inputs, *args, **kwargs):
     Groups = allocate_post_neuron_to_label(kwargs['local_synapse'], kwargs['label'])
     for ser, (data, l) in enumerate(zip(inputs, kwargs['label'])):
         for index, G in enumerate(Groups):
-            Switch = np.ones(args[index].N_post)
-            Switch[G[G['label'] == 0]['neuron']] = 0
+            Switch = np.zeros(args[index].N_post)
+            Switch[G[G['label'] == 1]['neuron'].tolist()[0]] = 1
             args[index].variables['Switch_plasticity'].set_value(Switch[args[index].j])
         weight_initial = [S.variables['w'].get_value().copy() for S in args]
         if ser % 50 == 0:
