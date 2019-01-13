d59dcbe5be988d6d6172ce3484710ce5ced7b96b
scarecrow <zhouyanasd@gmail.com>
2019-01-13 12:27:43 +0800

    large scale and multiple connection rules finish

--- Brian2_scripts/sim_brian_scratch/sim_brian_MNIST/sim_brian_MNIST_v7_CMA-ES_v1.py
+++ Brian2_scripts/sim_brian_scratch/sim_brian_MNIST/sim_brian_MNIST_v7_CMA-ES_v1.py
@@ -143,6 +143,57 @@ class Base():
         parameters = parameters.reshape(-1)
         return parameters
 
+    def set_local_parameter_PS(self, S, parameter, boundary = None, method='random', **kwargs):
+        if method == 'random':
+            random = rand(S.N_post) * (boundary[1]-boundary[0]) + boundary[0]
+            if '_post' in parameter:
+                S.variables[parameter].set_value(random)
+            else:
+                S.variables[parameter].set_value(random[S.j])
+        if method == 'group':
+            try:
+                group_n =  kwargs['group_parameters'].shape[0]
+                n = int(np.floor(S.N_post / group_n))
+                random = zeros(S.N_post)
+                for i in range(group_n):
+                    random[i * n:(i + 1) * n] = kwargs['group_parameters'][i]
+                for j in range(S.N_post - group_n*n):
+                    random[group_n * n + j:group_n * n + j + 1] = random[j * n]
+            except KeyError:
+                group_n = kwargs['group_n']
+                n = int(np.floor(S.N_post / group_n))
+                random = zeros(S.N_post)
+                for i in range(group_n):
+                    try:
+                        random[i * n:(i + 1) * n] = rand() * (boundary[1]-boundary[0]) + boundary[0]
+                    except IndexError:
+                        random[i * n:] = rand() * (boundary[1]-boundary[0]) + boundary[0]
+                        continue
+            if '_post' in parameter:
+                S.variables[parameter].set_value(random)
+            else:
+                S.variables[parameter].set_value(random[S.j])
+        if method == 'location':
+            group_n = kwargs['group_n']
+            location_label = kwargs['location_label']
+            random = zeros(S.N_post)
+            bound = np.linspace(0, max(S.variables[location_label].get_value() + 1), num=group_n + 1)
+            for i in range(group_n):
+                random[(S.variables[location_label].get_value() >= bound[i]) & (
+                            S.variables[location_label].get_value() < bound[i + 1])] \
+                    = rand() * (boundary[1]-boundary[0]) + boundary[0]
+            if '_post' in parameter:
+                S.variables[parameter].set_value(random)
+            else:
+                S.variables[parameter].set_value(random[S.j])
+        if method == 'in_coming':
+            max_incoming = max(S.N_incoming)
+            random = S.N_incoming / max_incoming * (boundary[1]-boundary[0]) + boundary[0]
+            if '_post' in parameter:
+                S.variables[parameter].set_value(random)
+            else:
+                S.variables[parameter].set_value(random[S.j])
+
 
 class Readout():
     def __init__(self, function):
@@ -363,7 +414,7 @@ class MNIST_classification(Base):
 
 ###################################
 # -----simulation parameter setting-------
-coding_n = 1
+coding_n = 2
 MNIST_shape = (1, 784)
 coding_duration = 30
 duration = coding_duration*MNIST_shape[0]
@@ -396,31 +447,41 @@ np_state = np.random.get_state()
 ############################################
 # ---- define network run function----
 def run_net(inputs, parameter):
+    """
+        run_net(inputs, parameter)
+            Parameters = [R, p_inE/I, f_in, f_EE, f_EI, f_IE, f_II, tau_ex, tau_inh]
+            ----------
+    """
+
     #---- set numpy random state for each run----
     np.random.set_state(np_state)
 
     # -----parameter setting-------
-    n_ex = 400
+    n_ex = 800
     n_inh = int(n_ex/4)
     n_input = MNIST_shape[1]*coding_n
     n_read = n_ex+n_inh
 
-    R = parameter[1]
-    f = parameter[2]
+    R = parameter[0]
+    f_in = parameter[2]
+    f_EE = parameter[3]
+    f_EI = parameter[4]
+    f_IE = parameter[5]
+    f_II = parameter[6]
 
-    A_EE = 30*f
-    A_EI = 60*f
-    A_IE = 19*f
-    A_II = 19*f
-    A_inE = 18*f
-    A_inI = 9*f
+    A_EE = 30*f_EE
+    A_EI = 60*f_EI
+    A_IE = 19*f_IE
+    A_II = 19*f_II
+    A_inE = 18*f_in
+    A_inI = 9*f_in
 
-    tau_ex = parameter[0]*coding_duration
-    tau_inh = parameter[0]*coding_duration
+    tau_ex = parameter[7:11]*coding_duration
+    tau_inh = parameter[11:15]*coding_duration
     tau_read= 30
 
-    p_inE = 0.01
-    p_inI = 0.01
+    p_inE = parameter[1]*0.02
+    p_inI = parameter[1]*0.02
 
     #------definition of equation-------------
     neuron_in = '''
@@ -496,8 +557,6 @@ def run_net(inputs, parameter):
     G_ex.h = '0'
     G_inh.h = '0'
     G_readout.h = '0'
-    G_ex.tau = tau_ex
-    G_inh.tau = tau_inh
     G_readout.tau = tau_read
 
     [G_ex,G_in] = base.allocate([G_ex,G_inh],5,5,20)
@@ -523,6 +582,9 @@ def run_net(inputs, parameter):
     S_EI.pre.delay = '0.8*ms'
     S_IE.pre.delay = '0.8*ms'
     S_II.pre.delay = '0.8*ms'
+
+    base.set_local_parameter(S_EE, 'tau_post', method='group', group_parameters=tau_ex)
+    base.set_local_parameter(S_II, 'tau_post', method='group', group_parameters=tau_inh)
 
     # ------create network-------------
     net = Network(collect())
@@ -565,5 +627,6 @@ def parameters_search(parameter):
 if __name__ == '__main__':
     core = 10
     pool = Pool(core)
-    res = cma.fmin(parameters_search, [0.5,0.9,1.2], 0.1, options={'ftarget': 1e-3,'bounds': [0, np.inf],
+    parameters = Parameters = [0.9, 0.5, 1.2, 1.2, 1.2, 1.2, 1.2, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]
+    res = cma.fmin(parameters_search, parameters, 0.25, options={'ftarget': 1e-3,'bounds': [0, np.inf],
                                                                  'maxiter':1000})
