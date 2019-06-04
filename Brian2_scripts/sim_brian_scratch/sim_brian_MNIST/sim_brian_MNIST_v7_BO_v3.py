681a1f0a9ebd6af9302721963c583b786c7b7961
scarecrow <zhouyanasd@gmail.com>
2019-06-04 16:07:47 +0800

    BOv3 update and add enhanced DE

--- Brian2_scripts/sim_brian_scratch/sim_brian_MNIST/sim_brian_MNIST_v7_BO_v3.py
+++ Brian2_scripts/sim_brian_scratch/sim_brian_MNIST/sim_brian_MNIST_v7_BO_v3.py
@@ -33,6 +33,10 @@ from bayes_opt.event import Events
 from bayes_opt.util import UtilityFunction
 from functools import partial
 
+from numba import njit
+from numpy import asarray, zeros, zeros_like, tile, array, argmin, mod
+from numpy.random import random, randint, rand, seed as rseed, uniform
+
 
 warnings.filterwarnings("ignore")
 prefs.codegen.target = "numpy"
@@ -42,6 +46,159 @@ data_path = '../../../Data/MNIST_data/'
 
 
 # ------define general function------------
+def wrap(v, vmin, vmax):
+    w = vmax - vmin
+    return vmin + mod(asarray(v) - vmin, w)
+
+
+def evolve_population(pop, pop2, bound, f, c):
+    npop, ndim = pop.shape
+
+    for i in range(npop):
+
+        # --- Vector selection ---
+        v1, v2, v3 = i, i, i
+        while v1 == i:
+            v1 = randint(npop)
+        while (v2 == i) or (v2 == v1):
+            v2 = randint(npop)
+        while (v3 == i) or (v3 == v2) or (v3 == v1):
+            v3 = randint(npop)
+
+        # --- Mutation ---
+        v = pop[v1] + f * (pop[v2] - pop[v3])
+        # random choice a value between when the solution out of the bounds
+        for a, b in zip(enumerate(v), bound):
+            if a[1] > b[1] or a[1] < b[0]:
+                v[a[0]] = np.random.uniform(b[0], b[1], 1)
+
+        # --- Cross over ---
+        co = rand(ndim)
+        for j in range(ndim):
+            if co[j] <= c:
+                pop2[i, j] = v[j]
+            else:
+                pop2[i, j] = pop[i, j]
+
+        # --- Forced crossing ---
+        j = randint(ndim)
+        pop2[i, j] = v[j]
+
+    return pop2
+
+
+class DiffEvol(object):
+
+    def __init__(self, fun, bounds, npop, f=None, c=None, seed=None, maximize=False, vectorize=False, cbounds=(0.25, 1),
+                 fbounds=(0.25, 0.75), pool=None, min_ptp=1e-2, args=[], kwargs={}):
+        if seed is not None:
+            np.random.rseed(seed)
+
+        self.minfun = _function_wrapper(fun, args, kwargs)
+        self.bounds = asarray(bounds)
+        self.n_pop = npop
+        self.n_par = self.bounds.shape[0]
+        self.bl = tile(self.bounds[:, 0], [npop, 1])
+        self.bw = tile(self.bounds[:, 1] - self.bounds[:, 0], [npop, 1])
+        self.m = -1 if maximize else 1
+        self.pool = pool
+        self.args = args
+
+        if self.pool is not None:
+            self.map = self.pool.map
+        else:
+            self.map = map
+
+        self.periodic = []
+        self.min_ptp = min_ptp
+
+        self.cmin = cbounds[0]
+        self.cmax = cbounds[1]
+        self.cbounds = cbounds
+        self.fbounds = fbounds
+
+        self.seed = seed
+        self.f = f
+        self.c = c
+
+        self._population = asarray(self.bl + random([self.n_pop, self.n_par]) * self.bw)
+        self._fitness = zeros(npop)
+        self._minidx = None
+
+        self._trial_pop = zeros_like(self._population)
+        self._trial_fit = zeros_like(self._fitness)
+
+        if vectorize:
+            self._eval = self._eval_vfun
+        else:
+            self._eval = self._eval_sfun
+
+    @property
+    def population(self):
+        """The parameter vector population"""
+        return self._population
+
+    @property
+    def minimum_value(self):
+        """The best-fit value of the optimized function"""
+        return self._fitness[self._minidx]
+
+    @property
+    def minimum_location(self):
+        """The best-fit solution"""
+        return self._population[self._minidx, :]
+
+    @property
+    def minimum_index(self):
+        """Index of the best-fit solution"""
+        return self._minidx
+
+    def optimize(self, ngen):
+        """Run the optimizer for ``ngen`` generations"""
+        res = 0
+        for res in self(ngen):
+            pass
+        return res
+
+    def __call__(self, ngen=1):
+        return self._eval(ngen)
+
+    def _eval_sfun(self, ngen=1):
+        """Run DE for a function that takes a single pv as an input and retuns a single value."""
+        popc, fitc = self._population, self._fitness
+        popt, fitt = self._trial_pop, self._trial_fit
+
+        for ipop in range(self.n_pop):
+            fitc[ipop] = self.m * self.minfun(popc[ipop, :])
+
+        for igen in range(ngen):
+            f = self.f or uniform(*self.fbounds)
+            c = self.c or uniform(*self.cbounds)
+
+            popt = evolve_population(popc, popt,self.bounds, f, c)
+            fitt[:] = self.m * array(list(self.map(self.minfun, popt)))
+
+            msk = fitt < fitc
+            popc[msk, :] = popt[msk, :]
+            fitc[msk] = fitt[msk]
+
+            self._minidx = argmin(fitc)
+            if fitc.ptp() < self.min_ptp:
+                break
+
+            yield popc[self._minidx, :], fitc[self._minidx]
+
+
+class _function_wrapper(object):
+    def __init__(self, f, args, kwargs):
+        self.f = f
+        self.args = args
+        self.kwargs = kwargs
+
+    def __call__(self, x):
+        return self.f(x, *self.args, **self.kwargs)
+
+
 class BayesianOptimization_(bayes_opt.BayesianOptimization):
     def __init__(self, f, pbounds, random_state=None, verbose=2):
         super(BayesianOptimization_, self).__init__(f, pbounds, random_state=None, verbose=2)
@@ -77,7 +234,7 @@ class BayesianOptimization_(bayes_opt.BayesianOptimization):
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
             self._gp.fit(self._space.params, self._space.target)
-        suggestion = self.acq_max_(
+        suggestion = self.acq_max_DE(
             ac=utility_function.utility,
             gp=self._gp,
             y_max=self._space.target.max(),
@@ -89,11 +246,19 @@ class BayesianOptimization_(bayes_opt.BayesianOptimization):
     def acq_max_(self,ac, gp, y_max, bounds, random_state):
         x_seeds = random_state.uniform(bounds[:, 0], bounds[:, 1],
                                        size=(bounds.shape[0]))
-        options = {'ftarget': 1e-3, 'bounds': bounds.T.tolist(), 'maxiter': 1000,
+        options = {'tolfunhist':-1e+4,'tolfun': -1e+4, 'ftarget': -1e+4, 'bounds': bounds.T.tolist(), 'maxiter': 1000,
                    'verb_log': 0,'verb_time':False,'verbose':-9}
         res = cma.fmin(lambda x: 1 - ac(x.reshape(1, -1), gp=gp, y_max=y_max), x_seeds, 0.25, options=options,
                        restarts=0, incpopsize=0, restart_from_best=False, bipop=False)
         x_max = res[0]
+        return np.clip(x_max, bounds[:, 0], bounds[:, 1])
+
+    def acq_max_DE(self, ac, gp, y_max, bounds, random_state, ngen=100, npop=45, f=0.4, c=0.3):
+        print(bounds)
+        de = DiffEvol(lambda x : 1 -ac(x.reshape(1, -1), gp=gp, y_max=y_max)[0], bounds, npop, f=f, c=c)
+        de.optimize(ngen)
+        print(de.minimum_value,de.minimum_location,de.minimum_index)
+        x_max = de.minimum_location
         return np.clip(x_max, bounds[:, 0], bounds[:, 1])
 
     def _prime_queue_LHS(self, init_points):
