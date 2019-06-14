44529e56c8361e525e494e3409e1d258b758dea9
scarecrow <zhouyanasd@gmail.com>
2019-06-14 16:24:34 +0800

    Start Building New topic CoE Program.

---
+++ Brian2_scripts/sim_brian_paper/sim_brian_paper_CoE/__init__.py
@@ -0,0 +1,12 @@
+from .core import *
+
+__all__ = [
+
+]
+
+__doc__ = """
+
+"""
+
+__author__ = 'Yan Zhou'
+__version__ = "0.1.0  $Revision: 001 $ $Date: 2019-06-14 13:13:52 +0200 (Fri, 14 Jun 2019) $"
---
+++ Brian2_scripts/sim_brian_paper/sim_brian_paper_CoE/core.py
@@ -0,0 +1,150 @@
+# -*- coding: utf-8 -*-
+"""
+    The basic function defined for the simulation.
+
+:Author: Yan Zhou
+
+:License: BSD 3-Clause, see LICENSE file.
+"""
+
+import os
+
+from functools import partial
+import struct
+import pickle
+from multiprocessing import Pool
+
+from brian2 import *
+import scipy as sp
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+
+import cma
+
+from bqplot import *
+import ipywidgets as widgets
+import matplotlib.pyplot as plt
+from brian2tools import *
+
+
+class Function():
+    """Some basic function for data transformation or calculation.
+
+    This class offers ....
+
+    Parameters
+    ----------
+
+    Attributes
+    ----------
+
+    Examples
+    --------
+
+    See also
+    --------
+
+    Notes
+    -----
+
+    """
+    def __init__(self):
+        pass
+
+class Base():
+    """Standardize a dataset along any axis
+
+    Center to the mean and component wise scale to unit variance.
+
+    Parameters
+    ----------
+    X : {array-like, sparse matrix}
+        The data to center and scale.
+
+    axis : int (0 by default)
+        axis used to compute the means and standard deviations along. If 0,
+        independently standardize each feature, otherwise (if 1) standardize
+        each sample.
+
+    Notes
+    -----
+
+    See also
+    --------
+    """
+    def __init__(self, duration, dt):
+        self.duration = duration
+        self.dt = dt
+        self.interval = duration * dt
+
+    def np_extend(self, a, b, axis=0):
+        if a is None:
+            shape = list(b.shape)
+            shape[axis] = 0
+            a = np.array([]).reshape(tuple(shape))
+        return np.append(a, b, axis)
+
+    def np_append(self, a, b):
+        shape = list(b.shape)
+        shape.insert(0, -1)
+        if a is None:
+            a = np.array([]).reshape(tuple(shape))
+        return np.append(a, b.reshape(tuple(shape)), axis=0)
+
+    def update_states(self, type='pandas', *args, **kwargs):
+        for seq, state in enumerate(kwargs):
+            if type == 'pandas':
+                kwargs[state] = kwargs[state].append(pd.DataFrame(args[seq]))
+            elif type == 'numpy':
+                kwargs[state] = self.np_extend(kwargs[state], args[seq], 1)
+        return kwargs
+
+    def allocate(self, G, X, Y, Z):
+        V = np.zeros((X, Y, Z), [('x', float), ('y', float), ('z', float)])
+        V['x'], V['y'], V['z'] = np.meshgrid(np.linspace(0, Y - 1, Y), np.linspace(0, X - 1, X),
+                                             np.linspace(0, Z - 1, Z))
+        V = V.reshape(X * Y * Z)
+        np.random.shuffle(V)
+        n = 0
+        for g in G:
+            for i in range(g.N):
+                g.x[i], g.y[i], g.z[i] = V[n][0], V[n][1], V[n][2]
+                n += 1
+        return G
+
+    def w_norm2(self, n_post, Synapsis):
+        for i in range(n_post):
+            a = Synapsis.w[np.where(Synapsis._synaptic_post == i)[0]]
+            Synapsis.w[np.where(Synapsis._synaptic_post == i)[0]] = a / np.linalg.norm(a)
+
+
+class Readout():
+    def __init__(self):
+        pass
+
+    def readout_sk(self, X_train, X_test, y_train, y_test, **kwargs):
+        lr = LogisticRegression(**kwargs)
+        lr.fit(X_train.T, y_train.T)
+        y_train_predictions = lr.predict(X_train.T)
+        y_test_predictions = lr.predict(X_test.T)
+        return accuracy_score(y_train_predictions, y_train.T), accuracy_score(y_test_predictions, y_test.T)
+
+
+class Result():
+    def __init__(self):
+        pass
+
+    def result_save(self, path, *arg, **kwarg):
+        if os.path.exists(path):
+            os.remove(path)
+        fw = open(path, 'wb')
+        pickle.dump(kwarg, fw)
+        fw.close()
+
+    def result_pick(self, path):
+        fr = open(path, 'rb')
+        data = pickle.load(fr)
+        fr.close()
+        return data
