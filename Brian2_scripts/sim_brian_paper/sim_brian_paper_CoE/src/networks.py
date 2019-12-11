d9ac974d2ba3125793af125329e3e432fa1ba8e1
scarecrow <zhouyanasd@gmail.com>
2019-12-11 17:31:30 +0800

    continue for network class

--- Brian2_scripts/sim_brian_paper/sim_brian_paper_CoE/src/networks.py
+++ Brian2_scripts/sim_brian_paper/sim_brian_paper_CoE/src/networks.py
@@ -14,20 +14,30 @@ from brian2 import *
 class Block():
     """Some basic function for data transformation or calculation.
 
-    This class offers ....
+    This class offers a basic property and functions of block in LSM.
 
     Parameters
     ----------
-    N: the number of neurons
-    Group
-    Synapse
+    N: int, the number of neurons
 
     Functions
     ----------
 
     """
-    def __init__(self, N):
-        pass
+    def __init__(self, N, equ, on_pre):
+        self.N = N
+
+    def create_neurons(self, model, threshold, reset,refractory):
+        self.neurons = NeuronGroup(self.N, self.equ, threshold='v > 0.9', reset='v = 0', method='linear',refractory=1*ms )
+
+    def create_synapse(self, ):
+        self.synapse = Synapses(self.neurons, self.neurons, 'w = 1 : 1',on_pre = self.on_pre, method='linear', delay = 1*ms)
+
+    def connect(self, connect_matrix):
+        self.synapse.connect(i=[],j=[])
+
+    def join_networks(self, net):
+        net.add(self.neurons, self.synapse)
 
 
 class Neuron():
