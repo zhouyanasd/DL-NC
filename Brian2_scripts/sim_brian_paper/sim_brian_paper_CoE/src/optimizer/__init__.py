7b69cd47e24e6bce02d7060098ec19db5de2cd21
scarecrow <zhouyanasd@gmail.com>
2020-06-02 11:13:35 +0800

    continue coe

---
+++ Brian2_scripts/sim_brian_paper/sim_brian_paper_CoE/src/optimizer/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+    The optimization methods used for NAS.
+
+:Author: Yan Zhou
+
+:License: BSD 3-Clause, see LICENSE file.
+"""
---
+++ Brian2_scripts/sim_brian_paper/sim_brian_paper_CoE/src/optimizer/bayesian.py
@@ -0,0 +1,439 @@
+from bayes_opt.event import Events
+from bayes_opt import BayesianOptimization
+from bayes_opt import UtilityFunction
+import numpy as np
+from .util import ensure_rng
+
+
+def _hashable(x):
+    """ ensure that an point is hashable by a python dict """
+    return tuple(map(float, x))
+
+
+class TargetSpace(object):
+    """
+    Holds the param-space coordinates (X) and target values (Y)
+    Allows for constant-time appends while ensuring no duplicates are added
+
+    Example
+    -------
+    >>> def target_func(p1, p2):
+    >>>     return p1 + p2
+    >>> pbounds = {'p1': (0, 1), 'p2': (1, 100)}
+    >>> space = TargetSpace(target_func, pbounds, random_state=0)
+    >>> x = space.random_points(1)[0]
+    >>> y = space.register_point(x)
+    >>> assert self.max_point()['max_val'] == y
+    """
+    def __init__(self, target_func, pbounds, random_state=None):
+        """
+        Parameters
+        ----------
+        target_func : function
+            Function to be maximized.
+
+        pbounds : dict
+            Dictionary with parameters names as keys and a tuple with minimum
+            and maximum values.
+
+        random_state : int, RandomState, or None
+            optionally specify a seed for a random number generator
+        """
+        self.random_state = ensure_rng(random_state)
+
+        # The function to be optimized
+        self.target_func = target_func
+
+        # Get the name of the parameters
+        self._keys = sorted(pbounds)
+        # Create an array with parameters bounds
+        self._bounds = np.array(
+            [item[1] for item in sorted(pbounds.items(), key=lambda x: x[0])],
+            dtype=np.float
+        )
+
+        # preallocated memory for X and Y points
+        self._params = np.empty(shape=(0, self.dim))
+        self._target = np.empty(shape=(0))
+
+        # keep track of unique points we have seen so far
+        self._cache = {}
+
+    def __contains__(self, x):
+        return _hashable(x) in self._cache
+
+    def __len__(self):
+        assert len(self._params) == len(self._target)
+        return len(self._target)
+
+    @property
+    def empty(self):
+        return len(self) == 0
+
+    @property
+    def params(self):
+        return self._params
+
+    @property
+    def target(self):
+        return self._target
+
+    @property
+    def dim(self):
+        return len(self._keys)
+
+    @property
+    def keys(self):
+        return self._keys
+
+    @property
+    def bounds(self):
+        return self._bounds
+
+    def params_to_array(self, params):
+        try:
+            assert set(params) == set(self.keys)
+        except AssertionError:
+            raise ValueError(
+                "Parameters' keys ({}) do ".format(sorted(params)) +
+                "not match the expected set of keys ({}).".format(self.keys)
+            )
+        return np.asarray([params[key] for key in self.keys])
+
+    def array_to_params(self, x):
+        try:
+            assert len(x) == len(self.keys)
+        except AssertionError:
+            raise ValueError(
+                "Size of array ({}) is different than the ".format(len(x)) +
+                "expected number of parameters ({}).".format(len(self.keys))
+            )
+        return dict(zip(self.keys, x))
+
+    def _as_array(self, x):
+        try:
+            x = np.asarray(x, dtype=float)
+        except TypeError:
+            x = self.params_to_array(x)
+
+        x = x.ravel()
+        try:
+            assert x.size == self.dim
+        except AssertionError:
+            raise ValueError(
+                "Size of array ({}) is different than the ".format(len(x)) +
+                "expected number of parameters ({}).".format(len(self.keys))
+            )
+        return x
+
+    def register(self, params, target):
+        """
+        Append a point and its target value to the known data.
+
+        Parameters
+        ----------
+        x : ndarray
+            a single point, with len(x) == self.dim
+
+        y : float
+            target function value
+
+        Raises
+        ------
+        KeyError:
+            if the point is not unique
+
+        Notes
+        -----
+        runs in ammortized constant time
+
+        Example
+        -------
+        >>> pbounds = {'p1': (0, 1), 'p2': (1, 100)}
+        >>> space = TargetSpace(lambda p1, p2: p1 + p2, pbounds)
+        >>> len(space)
+        0
+        >>> x = np.array([0, 0])
+        >>> y = 1
+        >>> space.add_observation(x, y)
+        >>> len(space)
+        1
+        """
+        x = self._as_array(params)
+        if x in self:
+            raise KeyError('Data point {} is not unique'.format(x))
+
+        # Insert data into unique dictionary
+        self._cache[_hashable(x.ravel())] = target
+
+        self._params = np.concatenate([self._params, x.reshape(1, -1)])
+        self._target = np.concatenate([self._target, [target]])
+
+    def probe(self, params):
+        """
+        Evaulates a single point x, to obtain the value y and then records them
+        as observations.
+
+        Notes
+        -----
+        If x has been previously seen returns a cached value of y.
+
+        Parameters
+        ----------
+        x : ndarray
+            a single point, with len(x) == self.dim
+
+        Returns
+        -------
+        y : float
+            target function value.
+        """
+        x = self._as_array(params)
+
+        try:
+            target = self._cache[_hashable(x)]
+        except KeyError:
+            params = dict(zip(self._keys, x))
+            target = self.target_func(**params)
+            self.register(x, target)
+        return target
+
+    def random_sample(self):
+        """
+        Creates random points within the bounds of the space.
+
+        Returns
+        ----------
+        data: ndarray
+            [num x dim] array points with dimensions corresponding to `self._keys`
+
+        Example
+        -------
+        >>> target_func = lambda p1, p2: p1 + p2
+        >>> pbounds = {'p1': (0, 1), 'p2': (1, 100)}
+        >>> space = TargetSpace(target_func, pbounds, random_state=0)
+        >>> space.random_points(1)
+        array([[ 55.33253689,   0.54488318]])
+        """
+        # TODO: support integer, category, and basic scipy.optimize constraints
+        data = np.empty((1, self.dim))
+        for col, (lower, upper) in enumerate(self._bounds):
+            data.T[col] = self.random_state.uniform(lower, upper, size=1)
+        return data.ravel()
+
+    def max(self):
+        """Get maximum target value found and corresponding parametes."""
+        try:
+            res = {
+                'target': self.target.max(),
+                'params': dict(
+                    zip(self.keys, self.params[self.target.argmax()])
+                )
+            }
+        except ValueError:
+            res = {}
+        return res
+
+    def res(self):
+        """Get all target values found and corresponding parametes."""
+        params = [dict(zip(self.keys, p)) for p in self.params]
+
+        return [
+            {"target": target, "params": param}
+            for target, param in zip(self.target, params)
+        ]
+
+    def set_bounds(self, new_bounds):
+        """
+        A method that allows changing the lower and upper searching bounds
+
+        Parameters
+        ----------
+        new_bounds : dict
+            A dictionary with the parameter name and its new bounds
+        """
+        for row, key in enumerate(self.keys):
+            if key in new_bounds:
+                self._bounds[row] = new_bounds[key]
+
+
+class Queue:
+    def __init__(self):
+        self._queue = []
+
+    @property
+    def empty(self):
+        return len(self) == 0
+
+    def __len__(self):
+        return len(self._queue)
+
+    def __next__(self):
+        if self.empty:
+            raise StopIteration("Queue is empty, no more objects to retrieve.")
+        obj = self._queue[0]
+        self._queue = self._queue[1:]
+        return obj
+
+    def next(self):
+        return self.__next__()
+
+    def add(self, obj):
+        """Add object to end of queue."""
+        self._queue.append(obj)
+
+
+class UtilityFunction_(UtilityFunction):
+    def __init__(self, kind, kappa, xi):
+        super(UtilityFunction_, self).__init__(kind, kappa, xi)
+
+    def utility(self, x, gp, y_min):
+        if self.kind == 'ucb':
+            return self._ucb_(x, gp, self.kappa)
+        if self.kind == 'ei':
+            return self._ei_(x, gp, y_min, self.xi)
+        if self.kind == 'poi':
+            return self._poi_(x, gp, y_min, self.xi)
+
+    @staticmethod
+    def _ucb_(x, gp, kappa):
+        mean, std = gp.predict(x, return_std=True)
+        return mean - kappa * std
+
+    @staticmethod
+    def _ei_(x, gp, y_min, xi):
+        mean, std = gp.predict(x, return_std=True)
+        z = (y_min - mean - xi) / std
+        return -(y_min - mean - xi) * norm.cdf(z) - std * norm.pdf(z)
+
+    @staticmethod
+    def _poi_(x, gp, y_min, xi):
+        mean, std = gp.predict(x, return_std=True)
+        z = (y_min - mean - xi) / std
+        return -norm.cdf(z)
+
+
+class BayesianOptimization_(BayesianOptimization):
+    def __init__(self, f, pbounds, random_state=None, verbose=0):
+        super(BayesianOptimization_, self).__init__(f, pbounds, random_state, verbose)
+
+    def _prime_queue_LHS(self, init_points):
+        """Make sure there's something in the queue at the very beginning."""
+        if self._queue.empty and self._space.empty:
+            init_points = max(init_points, 1)
+        LHS_points = self.LHSample(init_points, self._space.bounds)
+        for point in LHS_points:
+            self._queue.add(point)
+
+    def LHSample(self, N, bounds, D=None):
+        if D == None:
+            D = bounds.shape[0]
+        result = np.empty([N, D])
+        temp = np.empty([N])
+        d = 1.0 / N
+        for i in range(D):
+            for j in range(N):
+                temp[j] = np.random.uniform(
+                    low=j * d, high=(j + 1) * d, size=1)[0]
+            np.random.shuffle(temp)
+            for j in range(N):
+                result[j, i] = temp[j]
+        lower_bounds = bounds[:, 0]
+        upper_bounds = bounds[:, 1]
+        if np.any(lower_bounds > upper_bounds):
+            print('bounds error')
+            return None
+        np.add(np.multiply(result,
+                           (upper_bounds - lower_bounds),
+                           out=result),
+               lower_bounds,
+               out=result)
+        return result
+
+    def load_LHS(self, path):
+        X, fit = [], []
+        with open(path, 'r') as f:
+            l = f.readlines()
+        l.pop(0)
+        p1 = re.compile(r'[{](.*?)[}]', re.S)
+        for i in range(0, len(l)):
+            l[i] = l[i].rstrip('\n')
+            s = re.findall(p1, l[i])[0]
+            d = eval('{' + s + '}')
+            X.append(np.array(list(d.values())))
+            f = float(l[i].replace('{' + s + '}', '').split(' ')[2])
+            fit.append(f)
+        return X, fit
+
+    def acq_min_CMA(self, ac, gp, y_min, bounds, random_state):
+        x_seeds = random_state.uniform(bounds[:, 0], bounds[:, 1],
+                                       size=(bounds.shape[0]))
+        options = {'tolfunhist': -1e+4, 'tolfun': -1e+4, 'ftarget': -1e+4, 'bounds': bounds.T.tolist(), 'maxiter': 1000,
+                   'verb_log': 0, 'verb_time': False, 'verbose': -9}
+        res = cma.fmin(lambda x: ac(x.reshape(1, -1), gp=gp, y_min=y_min), x_seeds, 0.25, options=options,
+                       restarts=0, incpopsize=0, restart_from_best=False, bipop=False)
+        x_min = res[0]
+        return np.clip(x_min, bounds[:, 0], bounds[:, 1])
+
+    def acq_min_DE(self, ac, gp, y_min, bounds, random_state, ngen=100, npop=45, f=0.4, c=0.3):
+        de = DiffEvol(lambda x: ac(x.reshape(1, -1), gp=gp, y_min=y_min)[0], bounds, npop, f=f, c=c,
+                      seed=random_state)
+        de.optimize(ngen)
+        print(de.minimum_value, de.minimum_location, de.minimum_index)
+        x_min = de.minimum_location
+        return np.clip(x_min, bounds[:, 0], bounds[:, 1])
+
+    def suggest_(self, utility_function, opt_function):
+        if len(self._space) == 0:
+            return self._space.array_to_params(self._space.random_sample())
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            self._gp.fit(self._space.params, self._space.target)
+        suggestion = opt_function(
+            ac=utility_function.utility,
+            gp=self._gp,
+            y_min=self._space.target.min(),
+            bounds=self._space.bounds,
+            random_state=self._random_state.randint(100000)
+        )
+        return self._space.array_to_params(suggestion)
+
+    def guess_fixedpoint(self, utility_function, X):
+        gauss = utility_function.utility(X, self._gp, self._space.target.min())
+        return gauss
+
+    def minimize(self,
+                 LHS_path=None,
+                 init_points=5,
+                 is_LHS=False,
+                 n_iter=25,
+                 acq='ucb',
+                 opt=None,
+                 kappa=2.576,
+                 xi=0.0,
+                 **gp_params):
+        """Mazimize your function"""
+        self._prime_subscriptions()
+        self.dispatch(Events.OPTMIZATION_START)
+        if LHS_path == None:
+            if is_LHS:
+                self._prime_queue_LHS(init_points)
+            else:
+                self._prime_queue(init_points)
+        else:
+            X, fit = self.load_LHS(LHS_path)
+            for x, eva in zip(X, fit):
+                self.register(x, eva)
+        if opt == None:
+            opt = self.acq_min_DE
+        self.set_gp_params(**gp_params)
+        util = UtilityFunction_(kind=acq, kappa=kappa, xi=xi)
+        iteration = 0
+        while not self._queue.empty or iteration < n_iter:
+            try:
+                x_probe = next(self._queue)
+            except StopIteration:
+                x_probe = self.suggest_(util, opt)
+                iteration += 1
+            self.probe(x_probe, lazy=False)
+        self.dispatch(Events.OPTMIZATION_END)
