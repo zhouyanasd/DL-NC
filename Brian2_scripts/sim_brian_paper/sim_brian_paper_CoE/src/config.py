dba5e3fcfdf9bed2163492c696597e84ee63e65b
scarecrow <zhouyanasd@gmail.com>
2020-07-19 23:49:16 +0800

    generator and decoder：continue

--- Brian2_scripts/sim_brian_paper/sim_brian_paper_CoE/src/config.py
+++ Brian2_scripts/sim_brian_paper/sim_brian_paper_CoE/src/config.py
@@ -29,37 +29,40 @@ g += w * property_pre
 
 
 # --- parameter settings ---
-# Encoding = {}
-# Readout = {}
+Encoding = ['N']
+Readout = ['N']
 Reservoir = ['N', 'L_1', 'L_2']
 Block_random = ['N', 'tau', 'threshold', 'type', 'strength', 'plasticity', 'P']
 Block_scale_free = ['N', 'tau', 'threshold', 'type', 'strength', 'plasticity', 'C']
 Block_circle = ['N', 'tau', 'threshold', 'type', 'strength', 'plasticity', 'P_f', 'P_b', 'P_d', 'D_c']
 Block_hierarchy = ['N', 'tau', 'threshold', 'type', 'strength', 'plasticity', 'P_f', 'P_b', 'P_d', 'D_c', 'L']
 
-config_key = {'Reservoir': Reservoir, 'Block_random': Block_random, 'Block_scale_free': Block_scale_free,
-              'Block_circle': Block_circle, 'Block_hierarchy': Block_hierarchy}
-config_SubCom = {'Reservoir': [0, 1, 2], 'Block_random': [3, 4, 5, 6, 7, 8, 9],
-                 'Block_scale_free': [10, 11, 12, 13, 14, 15, 16],
-                 'Block_circle': [17, 18, 19, 20, 21, 22, 23, 24, 25, 26],
-                 'Block_hierarchy': [27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37]}
-config_codes = {'Reservoir': [None, 1, 1], 'Block_random': [None, None, None, None, None, None, None],
-                'Block_scale_free': [None, None, None, None, None, None, None],
-                'Block_circle': [None, None, None, None, None, None, None, None, None, None, None],
-                'Block_hierarchy': [None, None, None, None, None, None, None, None, None, None, None, None]}
-config_ranges = {'Reservoir': [[0, 1]] * 3, 'Block_random': [[0, 1]] * 7,
-                 'Block_scale_free': [[0, 1]] * 7,
-                 'Block_circle': [[0, 1]] * 10,
-                 'Block_hierarchy': [[0, 1]] * 11}
-config_borders = {'Reservoir': [[0, 1]] * 3, 'Block_random': [[0, 1]] * 7,
-                  'Block_scale_free': [[0, 1]] * 7,
-                  'Block_circle': [[0, 1]] * 10,
-                  'Block_hierarchy': [[0, 1]] * 11}
-config_precisions = {'Reservoir': [4, 0, 0], 'Block_random': [4] * 7,
-                     'Block_scale_free': [4] * 7,
-                     'Block_circle': [4] * 10,
-                     'Block_hierarchy': [4] * 11}
-config_scales = {'Reservoir': [0] * 3, 'Block_random': [4] * 7,
-                 'Block_scale_free': [0] * 7,
-                 'Block_circle': [0] * 10,
-                 'Block_hierarchy': [0] * 11}
+config_group = ['Encoding', 'Reservoir', 'Block_random', 'Block_scale_free',
+                'Block_circle', 'Block_hierarchy', 'Readout']
+
+config_key = {Encoding, Reservoir, Block_random, Block_scale_free,
+              Block_circle, Block_hierarchy, Readout}
+config_SubCom = [[38], [0, 1, 2], [3, 4, 5, 6, 7, 8, 9],
+                 [10, 11, 12, 13, 14, 15, 16],
+                 [17, 18, 19, 20, 21, 22, 23, 24, 25, 26],
+                 [27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37], [39]]
+config_codes = [[None], [None, 1, 1], [None, None, None, None, None, None, None],
+                [None, None, None, None, None, None, None],
+                [None, None, None, None, None, None, None, None, None, None, None],
+                [None, None, None, None, None, None, None, None, None, None, None, None], [None]]
+config_ranges = [[[0, 1]], [[0, 1]] * 3, [[0, 1]] * 7,
+                 [[0, 1]] * 7,
+                 [[0, 1]] * 10,
+                 [[0, 1]] * 11, [[0, 1]]]
+config_borders = [[[0, 1]], [[0, 1]] * 3, [[0, 1]] * 7,
+                  [[0, 1]] * 7,
+                  [[0, 1]] * 10,
+                  [[0, 1]] * 11, [[0, 1]]]
+config_precisions = [[0], [4, 0, 0], [4] * 7,
+                     [4] * 7,
+                     [4] * 10,
+                     [4] * 11, [0]]
+config_scales = [[0], [0] * 3, [0] * 7,
+                 [0] * 7,
+                 [0] * 10,
+                 [0] * 11, [0]]
--- Brian2_scripts/sim_brian_paper/sim_brian_paper_CoE/src/networks/decoder.py
+++ Brian2_scripts/sim_brian_paper/sim_brian_paper_CoE/src/networks/decoder.py
@@ -13,8 +13,10 @@ import numpy as np
 import geatpy as ga
 
 class Decoder(BaseFunctions):
-    def __init__(self, config_key, config_SubCom, config_codes, config_ranges, config_borders,
+    def __init__(self, config_group, config_key, config_SubCom, config_codes, config_ranges, config_borders,
                       config_precisions, config_scales):
+        super.__init__()
+        self.config_group = config_group
         self.config_key = config_key
         self.config_codes = config_codes
         self.config_SubCom = config_SubCom
@@ -26,14 +28,14 @@ class Decoder(BaseFunctions):
     @property
     def get_keys(self):
         keys = []
-        for group_name in self.config_key.keys():
-            for key in self.config_key[group_name]:
+        for index, group_name in enumerate(self.config_group):
+            for key in self.config_key[index]:
                 keys.append(group_name+'_'+key)
         return keys
 
     @property
     def get_SubCom(self):
-        return list(self.config_SubCom.values())
+        return self.config_SubCom
 
     @property
     def get_dim(self):
@@ -42,35 +44,35 @@ class Decoder(BaseFunctions):
     @property
     def get_codes(self):
         codes = []
-        for group_codes in self.config_codes.values():
+        for group_codes in self.config_codes:
             codes.extend(group_codes)
         return np.array(codes)
 
     @property
     def get_ranges(self):
         ranges = []
-        for group_ranges in self.config_ranges.values():
+        for group_ranges in self.config_ranges:
             ranges.extend(group_ranges)
         return np.array(ranges).T
 
     @property
     def get_borders(self):
         borders = []
-        for group_borders in self.config_borders.values():
+        for group_borders in self.config_borders:
             borders.extend(group_borders)
         return np.array(borders).T
 
     @property
     def get_precisions(self):
         precisions = []
-        for group_precisions in self.config_precisions.values():
+        for group_precisions in self.config_precisions:
             precisions.extend(group_precisions)
         return np.array(precisions)
 
     @property
     def get_scales(self):
         scales = []
-        for group_scales in self.config_scales.values():
+        for group_scales in self.config_scales:
             scales.extend(group_scales)
         return np.array(scales)
 
@@ -78,10 +80,12 @@ class Decoder(BaseFunctions):
         self.Gen = Gen
 
     def decode(self, target):
-        key = self.config_key[target]
-        SubCom = self.config_SubCom[target]
+        group = np.where(self.config_group == target)[0]
+        key = self.config_key[group]
+        SubCom = self.config_SubCom[group]
+        codes = self.config_codes[group]
         parameter = {}
-        for p,k,c in zip(self.Gen[SubCom],key, self.codes):
+        for p,k,c in zip(self.Gen[SubCom], key, codes):
             if c != None:
                 p = self.dec2bin(p)
             parameter[k] = p
@@ -90,40 +94,43 @@ class Decoder(BaseFunctions):
 
 
 if __name__ == "__main__":
-    # Encoding = {}
-    # Readout = {}
+    Encoding = ['N']
+    Readout = ['N']
     Reservoir = ['N', 'L_1', 'L_2']
     Block_random = ['N', 'tau', 'threshold', 'type', 'strength', 'plasticity', 'P']
     Block_scale_free = ['N', 'tau', 'threshold', 'type', 'strength', 'plasticity', 'C']
     Block_circle = ['N', 'tau', 'threshold', 'type', 'strength', 'plasticity', 'P_f', 'P_b', 'P_d', 'D_c']
     Block_hierarchy = ['N', 'tau', 'threshold', 'type', 'strength', 'plasticity', 'P_f', 'P_b', 'P_d', 'D_c', 'L']
 
-    config_key = {'Reservoir':Reservoir, 'Block_random':Block_random, 'Block_scale_free':Block_scale_free,
-              'Block_circle':Block_circle,'Block_hierarchy':Block_hierarchy}
-    config_SubCom = {'Reservoir':[0, 1, 2], 'Block_random':[3, 4, 5, 6, 7, 8, 9],
-                     'Block_scale_free': [10, 11, 12, 13, 14, 15, 16],
-                     'Block_circle': [17,18,19, 20, 21, 22, 23,24,25,26],
-                     'Block_hierarchy': [27,28,29,30,31,32,33,34,35,36,37]}
-    config_codes = {'Reservoir':[None, 1, 1], 'Block_random':[None, None, None, None, None, None, None],
-                     'Block_scale_free': [None, None, None, None, None, None, None],
-                     'Block_circle': [None, None, None, None, None, None, None, None, None, None, None],
-                     'Block_hierarchy': [None, None, None, None, None, None, None, None, None, None, None, None]}
-    config_ranges = {'Reservoir':[[0, 1]]*3, 'Block_random':[[0, 1]]*7,
-                     'Block_scale_free': [[0, 1]]*7,
-                     'Block_circle': [[0, 1]]*10,
-                     'Block_hierarchy': [[0, 1]]*11}
-    config_borders = {'Reservoir':[[0, 1]]*3, 'Block_random':[[0, 1]]*7,
-                     'Block_scale_free': [[0, 1]]*7,
-                     'Block_circle': [[0, 1]]*10,
-                     'Block_hierarchy': [[0, 1]]*11}
-    config_precisions = {'Reservoir':[4, 0, 0], 'Block_random':[4]*7,
-                     'Block_scale_free': [4]*7,
-                     'Block_circle': [4]*10,
-                     'Block_hierarchy': [4]*11}
-    config_scales = {'Reservoir':[0]*3, 'Block_random':[4]*7,
-                     'Block_scale_free': [0]*7,
-                     'Block_circle': [0]*10,
-                     'Block_hierarchy': [0]*11}
+    config_group = ['Encoding', 'Reservoir', 'Block_random', 'Block_scale_free',
+                    'Block_circle','Block_hierarchy', 'Readout']
 
-    decoder = Decoder(config_key, config_SubCom, config_codes, config_ranges, config_borders,
+    config_key = {Encoding, Reservoir, Block_random, Block_scale_free,
+                  Block_circle, Block_hierarchy, Readout}
+    config_SubCom = [[38],[0, 1, 2], [3, 4, 5, 6, 7, 8, 9],
+                     [10, 11, 12, 13, 14, 15, 16],
+                     [17,18,19, 20, 21, 22, 23,24,25,26],
+                     [27,28,29,30,31,32,33,34,35,36,37],[39]]
+    config_codes = [[None],[None, 1, 1], [None, None, None, None, None, None, None],
+                     [None, None, None, None, None, None, None],
+                     [None, None, None, None, None, None, None, None, None, None, None],
+                     [None, None, None, None, None, None, None, None, None, None, None, None],[None]]
+    config_ranges = [[[0,1]],[[0, 1]]*3, [[0, 1]]*7,
+                     [[0, 1]]*7,
+                     [[0, 1]]*10,
+                     [[0, 1]]*11,[[0,1]]]
+    config_borders = [[[0,1]],[[0, 1]]*3, [[0, 1]]*7,
+                     [[0, 1]]*7,
+                     [[0, 1]]*10,
+                     [[0, 1]]*11,[[0,1]]]
+    config_precisions = [[0], [4, 0, 0], [4]*7,
+                     [4]*7,
+                     [4]*10,
+                     [4]*11,[0]]
+    config_scales = [[0],[0]*3, [0]*7,
+                     [0]*7,
+                     [0]*10,
+                     [0]*11,[0]]
+
+    decoder = Decoder(config_group, config_key, config_SubCom, config_codes, config_ranges, config_borders,
                       config_precisions, config_scales)
