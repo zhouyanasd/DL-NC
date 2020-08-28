a73f2ca3b9fcaf12e25ed24d101cad08cfbc756a
scarecrow <zhouyanasd@gmail.com>
2020-08-28 11:53:35 +0800

    optimizor: start random forest

--- Brian2_scripts/sim_brian_paper/sim_brian_paper_CoE/src/optimizer/random_forest.py
+++ Brian2_scripts/sim_brian_paper/sim_brian_paper_CoE/src/optimizer/random_forest.py
@@ -1,203 +1,334 @@
-# -*- coding: utf-8 -*-
-"""
-@Env: Python2.7
-@Time: 2019/10/24 14:22
-@Author: zhaoxingfeng
-@Function：Random Forest（RF），随机森林回归
-@Version: V1.2
-"""
-
 import numpy as np
-import random
-import math
-from sklearn.externals.joblib import Parallel, delayed
 
 
-class Tree(object):
-    """定义一棵决策树"""
-
-    def __init__(self):
-        self.split_feature = None
-        self.split_value = None
-        self.leaf_value = None
-        self.tree_left = None
-        self.tree_right = None
-
-    def calc_predict_value(self, dataset):
-        """通过递归决策树找到样本所属叶子节点"""
-        if self.leaf_value is not None:
-            return self.leaf_value
-        elif dataset[self.split_feature] <= self.split_value:
-            return self.tree_left.calc_predict_value(dataset)
-        else:
-            return self.tree_right.calc_predict_value(dataset)
-
-    def describe_tree(self):
-        """以json形式打印决策树，方便查看树结构"""
-        if not self.tree_left and not self.tree_right:
-            leaf_info = "{leaf_value:" + str(self.leaf_value) + "}"
-            return leaf_info
-        left_info = self.tree_left.describe_tree()
-        right_info = self.tree_right.describe_tree()
-        tree_structure = "{split_feature:" + str(self.split_feature) + \
-                         ",split_value:" + str(self.split_value) + \
-                         ",left_tree:" + left_info + \
-                         ",right_tree:" + right_info + "}"
-        return tree_structure
+def mse(y):
+    """
+    Mean squared error for decision tree (ie., mean) predictions
+    """
+    return np.mean((y - np.mean(y)) ** 2)
 
 
-class RandomForestRegression(object):
-    def __init__(self, n_estimators=10, max_depth=-1, min_samples_split=2, min_samples_leaf=1,
-                 min_split_gain=0.0, colsample_bytree=None, subsample=0.8, random_state=None):
+def entropy(y):
+    """
+    Entropy of a label sequence
+    """
+    hist = np.bincount(y)
+    ps = hist / np.sum(hist)
+    return -np.sum([p * np.log2(p) for p in ps if p > 0])
+
+
+def gini(y):
+    """
+    Gini impurity (local entropy) of a label sequence
+    """
+    hist = np.bincount(y)
+    N = np.sum(hist)
+    return 1 - sum([(i / N) ** 2 for i in hist])
+
+
+
+def bootstrap_sample(X, Y):
+    N, M = X.shape
+    idxs = np.random.choice(N, N, replace=True)
+    return X[idxs], Y[idxs]
+
+
+class Node:
+    def __init__(self, left, right, rule):
+        self.left = left
+        self.right = right
+        self.feature = rule[0]
+        self.threshold = rule[1]
+
+
+class Leaf:
+    def __init__(self, value):
         """
-        随机森林参数
+        `value` is an array of class probabilities if classifier is True, else
+        the mean of the region
+        """
+        self.value = value
+
+
+class DecisionTree:
+    def __init__(
+        self,
+        classifier=True,
+        max_depth=None,
+        n_feats=None,
+        criterion="entropy",
+        seed=None,
+    ):
+        """
+        A decision tree model for regression and classification problems.
+
+        Parameters
         ----------
-        n_estimators:      树数量
-        max_depth:         树深度，-1表示不限制深度
-        min_samples_split: 节点分裂所需的最小样本数量，小于该值节点终止分裂
-        min_samples_leaf:  叶子节点最少样本数量，小于该值叶子被合并
-        min_split_gain:    分裂所需的最小增益，小于该值节点终止分裂
-        colsample_bytree:  列采样设置，可取[sqrt、log2]。sqrt表示随机选择sqrt(n_features)个特征，
-                           log2表示随机选择log(n_features)个特征，设置为其他则不进行列采样
-        subsample:         行采样比例
-        random_state:      随机种子，设置之后每次生成的n_estimators个样本集不会变，确保实验可重复
+        classifier : bool
+            Whether to treat target values as categorical (classifier =
+            True) or continuous (classifier = False). Default is True.
+        max_depth: int or None
+            The depth at which to stop growing the tree. If None, grow the tree
+            until all leaves are pure. Default is None.
+        n_feats : int
+            Specifies the number of features to sample on each split. If None,
+            use all features on each split. Default is None.
+        criterion : {'mse', 'entropy', 'gini'}
+            The error criterion to use when calculating splits. When
+            `classifier` is False, valid entries are {'mse'}. When `classifier`
+            is True, valid entries are {'entropy', 'gini'}. Default is
+            'entropy'.
+        seed : int or None
+            Seed for the random number generator. Default is None.
         """
-        self.n_estimators = n_estimators
-        self.max_depth = max_depth if max_depth != -1 else float('inf')
-        self.min_samples_split = min_samples_split
-        self.min_samples_leaf = min_samples_leaf
-        self.min_split_gain = min_split_gain
-        self.colsample_bytree = colsample_bytree
-        self.subsample = subsample
-        self.random_state = random_state
-        self.trees = None
-        self.feature_importances_ = dict()
+        if seed:
+            np.random.seed(seed)
 
-    def fit(self, data, targets):
-        """模型训练入口"""
-        if self.random_state:
-            random.seed(self.random_state)
-        random_state_stages = random.sample(range(self.n_estimators), self.n_estimators)
+        self.depth = 0
+        self.root = None
 
-        # 两种列采样方式
-        if self.colsample_bytree == "sqrt":
-            self.colsample_bytree = int(len(data) ** 0.5)
-        elif self.colsample_bytree == "log2":
-            self.colsample_bytree = int(math.log(len(data)))
+        self.n_feats = n_feats
+        self.criterion = criterion
+        self.classifier = classifier
+        self.max_depth = max_depth if max_depth else np.inf
+
+        if not classifier and criterion in ["gini", "entropy"]:
+            raise ValueError(
+                "{} is a valid criterion only when classifier = True.".format(criterion)
+            )
+        if classifier and criterion == "mse":
+            raise ValueError("`mse` is a valid criterion only when classifier = False.")
+
+    def fit(self, X, Y):
+        """
+        Fit a binary decision tree to a dataset.
+
+        Parameters
+        ----------
+        X : :py:class:`ndarray <numpy.ndarray>` of shape `(N, M)`
+            The training data of `N` examples, each with `M` features
+        Y : :py:class:`ndarray <numpy.ndarray>` of shape `(N,)`
+            An array of integer class labels for each example in `X` if
+            self.classifier = True, otherwise the set of target values for
+            each example in `X`.
+        """
+        self.n_classes = max(Y) + 1 if self.classifier else None
+        self.n_feats = X.shape[1] if not self.n_feats else min(self.n_feats, X.shape[1])
+        self.root = self._grow(X, Y)
+
+    def predict(self, X):
+        """
+        Use the trained decision tree to classify or predict the examples in `X`.
+
+        Parameters
+        ----------
+        X : :py:class:`ndarray <numpy.ndarray>` of shape `(N, M)`
+            The training data of `N` examples, each with `M` features
+
+        Returns
+        -------
+        preds : :py:class:`ndarray <numpy.ndarray>` of shape `(N,)`
+            The integer class labels predicted for each example in `X` if
+            self.classifier = True, otherwise the predicted target values.
+        """
+        return np.array([self._traverse(x, self.root) for x in X])
+
+    def predict_class_probs(self, X):
+        """
+        Use the trained decision tree to return the class probabilities for the
+        examples in `X`.
+
+        Parameters
+        ----------
+        X : :py:class:`ndarray <numpy.ndarray>` of shape `(N, M)`
+            The training data of `N` examples, each with `M` features
+
+        Returns
+        -------
+        preds : :py:class:`ndarray <numpy.ndarray>` of shape `(N, n_classes)`
+            The class probabilities predicted for each example in `X`.
+        """
+        assert self.classifier, "`predict_class_probs` undefined for classifier = False"
+        return np.array([self._traverse(x, self.root, prob=True) for x in X])
+
+    def _grow(self, X, Y, cur_depth=0):
+        # if all labels are the same, return a leaf
+        if len(set(Y)) == 1:
+            if self.classifier:
+                prob = np.zeros(self.n_classes)
+                prob[Y[0]] = 1.0
+            return Leaf(prob) if self.classifier else Leaf(Y[0])
+
+        # if we have reached max_depth, return a leaf
+        if cur_depth >= self.max_depth:
+            v = np.mean(Y, axis=0)
+            if self.classifier:
+                v = np.bincount(Y, minlength=self.n_classes) / len(Y)
+            return Leaf(v)
+
+        cur_depth += 1
+        self.depth = max(self.depth, cur_depth)
+
+        N, M = X.shape
+        feat_idxs = np.random.choice(M, self.n_feats, replace=False)
+
+        # greedily select the best split according to `criterion`
+        feat, thresh = self._segment(X, Y, feat_idxs)
+        l = np.argwhere(X[:, feat] <= thresh).flatten()
+        r = np.argwhere(X[:, feat] > thresh).flatten()
+
+        # grow the children that result from the split
+        left = self._grow(X[l, :], Y[l], cur_depth)
+        right = self._grow(X[r, :], Y[r], cur_depth)
+        return Node(left, right, (feat, thresh))
+
+    def _segment(self, X, Y, feat_idxs):
+        """
+        Find the optimal split rule (feature index and split threshold) for the
+        data according to `self.criterion`.
+        """
+        best_gain = -np.inf
+        split_idx, split_thresh = None, None
+        for i in feat_idxs:
+            vals = X[:, i]
+            levels = np.unique(vals)
+            thresholds = (levels[:-1] + levels[1:]) / 2 if len(levels) > 1 else levels
+            gains = np.array([self._impurity_gain(Y, t, vals) for t in thresholds])
+
+            if gains.max() > best_gain:
+                split_idx = i
+                best_gain = gains.max()
+                split_thresh = thresholds[gains.argmax()]
+
+        return split_idx, split_thresh
+
+    def _impurity_gain(self, Y, split_thresh, feat_values):
+        """
+        Compute the impurity gain associated with a given split.
+
+        IG(split) = loss(parent) - weighted_avg[loss(left_child), loss(right_child)]
+        """
+        if self.criterion == "entropy":
+            loss = entropy
+        elif self.criterion == "gini":
+            loss = gini
+        elif self.criterion == "mse":
+            loss = mse
+
+        parent_loss = loss(Y)
+
+        # generate split
+        left = np.argwhere(feat_values <= split_thresh).flatten()
+        right = np.argwhere(feat_values > split_thresh).flatten()
+
+        if len(left) == 0 or len(right) == 0:
+            return 0
+
+        # compute the weighted avg. of the loss for the children
+        n = len(Y)
+        n_l, n_r = len(left), len(right)
+        e_l, e_r = loss(Y[left]), loss(Y[right])
+        child_loss = (n_l / n) * e_l + (n_r / n) * e_r
+
+        # impurity gain is difference in loss before vs. after split
+        ig = parent_loss - child_loss
+        return ig
+
+    def _traverse(self, X, node, prob=False):
+        if isinstance(node, Leaf):
+            if self.classifier:
+                return node.value if prob else node.value.argmax()
+            return node.value
+        if X[node.feature] <= node.threshold:
+            return self._traverse(X, node.left, prob)
+        return self._traverse(X, node.right, prob)
+
+
+class RandomForest:
+    """
+    A random forest of decision trees.
+    """
+
+    def __init__(
+        self, n_trees, max_depth, n_feats, classifier=True, criterion="entropy"
+    ):
+        """
+        An ensemble (forest) of decision trees where each split is calculated
+        using a random subset of the features in the input.
+
+        Parameters
+        ----------
+        n_trees : int
+            The number of individual decision trees to use within the ensemble.
+        max_depth: int or None
+            The depth at which to stop growing each decision tree. If `None`,
+            grow each tree until the leaf nodes are pure.
+        n_feats : int
+            The number of features to sample on each split.
+        criterion : str (default: 'entropy')
+            The error criterion to use when calculating splits. Valid entries
+            are {'entropy', 'gini'}.
+        """
+        self.trees = []
+        self.n_trees = n_trees
+        self.n_feats = n_feats
+        self.max_depth = max_depth
+        self.criterion = criterion
+        self.classifier = classifier
+
+    def fit(self, X, Y):
+        """
+        Create `n_trees`-worth of bootstrapped samples from the training data
+        and use each to fit a separate decision tree.
+        """
+        self.trees = []
+        for _ in range(self.n_trees):
+            X_samp, Y_samp = bootstrap_sample(X, Y)
+            tree = DecisionTree(
+                n_feats=self.n_feats,
+                max_depth=self.max_depth,
+                criterion=self.criterion,
+                classifier=self.classifier,
+            )
+            tree.fit(X_samp, Y_samp)
+            self.trees.append(tree)
+
+    def predict(self, X):
+        """
+        Predict the target value for each entry in X.
+
+        Parameters
+        ----------
+        X : numpy array of shape (N, M)
+            The training data of N examples, each with M features
+
+        Returns
+        -------
+        y_pred : np.array of shape (N,)
+            Model predictions for each entry in X.
+        """
+        tree_preds = np.array([[t._traverse(x, t.root) for x in X] for t in self.trees])
+        return self._vote(tree_preds)
+
+    def _vote(self, predictions):
+        """
+        Return the aggregated prediction across all trees in the RF for each problem.
+
+        Parameters
+        ----------
+        predictions : np.array of shape (n_trees, N)
+            The array of predictions from each decision tree in the RF for each
+            of the N problems in X
+
+        Returns
+        -------
+        y_pred : np.array of shape (N,)
+            If classifier is True, the class label predicted by the majority of
+            the decision trees for each problem in X. If classifier is False,
+            the average prediction across decision trees on each problem.
+        """
+        if self.classifier:
+            out = [np.bincount(x).argmax() for x in predictions.T]
         else:
-            self.colsample_bytree = len(data)
-
-        # 并行建立多棵决策树
-        self.trees = Parallel(n_jobs=-1, verbose=0, backend="threading")(
-            delayed(self._parallel_build_trees)(data, targets, random_state)
-            for random_state in random_state_stages)
-
-    def _parallel_build_trees(self, data, targets, random_state):
-        """bootstrap有放回抽样生成训练样本集，建立决策树"""
-        subcol_index = random.sample(data, self.colsample_bytree)
-        data_stage = data.sample(n=int(self.subsample * len(data)), replace=True,
-                                       random_state=random_state).reset_index(drop=True)
-        data_stage = data_stage[:, subcol_index]
-        targets_stage = targets.sample(n=int(self.subsample * len(data)), replace=True,
-                                       random_state=random_state).reset_index(drop=True)
-
-        tree = self._build_single_tree(data_stage, targets_stage, depth=0)
-        print(tree.describe_tree())
-        return tree
-
-    def _build_single_tree(self, dataset, targets, depth):
-        """递归建立决策树"""
-        # 如果该节点的类别全都一样/样本小于分裂所需最小样本数量，则选取出现次数最多的类别。终止分裂
-        if len(targets['label'].unique()) <= 1 or dataset.__len__() <= self.min_samples_split:
-            tree = Tree()
-            tree.leaf_value = self.calc_leaf_value(targets['label'])
-            return tree
-
-        if depth < self.max_depth:
-            best_split_feature, best_split_value, best_split_gain = self.choose_best_feature(dataset, targets)
-            left_dataset, right_dataset, left_targets, right_targets = \
-                self.split_dataset(dataset, targets, best_split_feature, best_split_value)
-
-            tree = Tree()
-            # 如果父节点分裂后，左叶子节点/右叶子节点样本小于设置的叶子节点最小样本数量，则该父节点终止分裂
-            if left_dataset.__len__() <= self.min_samples_leaf or \
-                    right_dataset.__len__() <= self.min_samples_leaf or \
-                    best_split_gain <= self.min_split_gain:
-                tree.leaf_value = self.calc_leaf_value(targets['label'])
-                return tree
-            else:
-                # 如果分裂的时候用到该特征，则该特征的importance加1
-                self.feature_importances_[best_split_feature] = \
-                    self.feature_importances_.get(best_split_feature, 0) + 1
-
-                tree.split_feature = best_split_feature
-                tree.split_value = best_split_value
-                tree.tree_left = self._build_single_tree(left_dataset, left_targets, depth + 1)
-                tree.tree_right = self._build_single_tree(right_dataset, right_targets, depth + 1)
-                return tree
-        # 如果树的深度超过预设值，则终止分裂
-        else:
-            tree = Tree()
-            tree.leaf_value = self.calc_leaf_value(targets['label'])
-            return tree
-
-    def choose_best_feature(self, dataset, targets):
-        """寻找最好的数据集划分方式，找到最优分裂特征、分裂阈值、分裂增益"""
-        best_split_gain = float("inf")
-        best_split_feature = None
-        best_split_value = None
-
-        for feature in dataset.columns:
-            if dataset[feature].unique().__len__() <= 100:
-                unique_values = sorted(dataset[feature].unique().tolist())
-            # 如果该维度特征取值太多，则选择100个百分位值作为待选分裂阈值
-            else:
-                unique_values = np.unique([np.percentile(dataset[feature], x)
-                                           for x in np.linspace(0, 100, 100)])
-
-            # 对可能的分裂阈值求分裂增益，选取增益最大的阈值
-            for split_value in unique_values:
-                left_targets = targets[dataset[feature] <= split_value]
-                right_targets = targets[dataset[feature] > split_value]
-                split_gain = self.calc_r2(left_targets['label'], right_targets['label'])
-
-                if split_gain < best_split_gain:
-                    best_split_feature = feature
-                    best_split_value = split_value
-                    best_split_gain = split_gain
-        return best_split_feature, best_split_value, best_split_gain
-
-    @staticmethod
-    def calc_leaf_value(targets):
-        """选择所有样本的均值作为叶子节点取值"""
-        return targets.mean()
-
-    @staticmethod
-    def calc_r2(left_targets, right_targets):
-        """回归树采用平方误差作为指标来选择最优分裂点"""
-        r2 = 0
-        for targets in [left_targets, right_targets]:
-            mean = targets.mean()
-            for dt in targets:
-                r2 += (dt - mean) ** 2
-        return r2
-
-    @staticmethod
-    def split_dataset(dataset, targets, split_feature, split_value):
-        """根据特征和阈值将样本划分成左右两份，左边小于等于阈值，右边大于阈值"""
-        left_dataset = dataset[dataset[split_feature] <= split_value]
-        left_targets = targets[dataset[split_feature] <= split_value]
-        right_dataset = dataset[dataset[split_feature] > split_value]
-        right_targets = targets[dataset[split_feature] > split_value]
-        return left_dataset, right_dataset, left_targets, right_targets
-
-    def predict(self, dataset):
-        """输入样本，得到预测值"""
-        res = []
-        for _, row in dataset.iterrows():
-            pred_list = []
-            # 统计每棵树的预测结果，再求平均作为最终预测值
-            for tree in self.trees:
-                pred_list.append(tree.calc_predict_value(row))
-            res.append(sum(pred_list) * 1.0 / len(pred_list))
-        return np.array(res)
+            out = [np.mean(x) for x in predictions.T]
+        return np.array(out)
