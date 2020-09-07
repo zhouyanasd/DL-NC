05b5ba117f5ba747bee7dcb48be670cd3a06a413
scarecrow <zhouyanasd@gmail.com>
2020-09-07 14:37:36 +0800

    Main programme：adjust the main file

--- Brian2_scripts/sim_brian_paper/sim_brian_paper_CoE/KTH_CoE_v1.py
+++ Brian2_scripts/sim_brian_paper/sim_brian_paper_CoE/KTH_CoE_v1.py
@@ -123,6 +123,7 @@ if __name__ == '__main__':
     pool = Pool(core)
 
     method = 'CoE'
+    surrogate = 'rf'
 
 # -------parameters search---------------
     if method == 'BO':
@@ -137,71 +138,6 @@ if __name__ == '__main__':
 
 
 
-# --- parameters needs to be update by optimizer ---
-# connect_matrix_block = np.array([[1,1,2,2],[3,5,4,6]])
-# connect_matrix_blocks = [connect_matrix_block]*blocks_reservoir
-# connect_matrix_reservoir = np.array([[1,2,3,4],[5,6,7,8]])
-#
-# tau_neurons = 30
-#
-# strength_synapse_block = np.random.rand((neurons_block*neurons_block))
 # delay_synapse_block = np.random.rand((neurons_block*neurons_block)) * ms
-#
-# strength_synapse_reservoir = np.random.rand((blocks_reservoir*blocks_reservoir))
-# delay_synapse_reservoir = np.random.rand((blocks_reservoir*blocks_reservoir)) * ms
-#
-# strength_synapse_encoding_reservoir = np.random.rand
-
-##-----------------------------------------------------
-#--- create network ---
-# net = LSM_Network()
-#
-# #--- create encoding and readout neurons ---
-# encoding = NeuronGroup(neurons_encoding, dynamics_encoding, threshold = 'I > 0', method = 'euler', refractory = 0 * ms,
-#                         name = 'encoding')
-# net.register_layer(encoding,'encoding')
-#
-# readout = NeuronGroup(neurons_block * blocks_reservoir, dynamics_readout, method = 'euler', name  = 'readout')
-# net.register_layer(readout,'readout')
-#
-# #--- create reservoir ---
-# reservoir = Reservoir(blocks_reservoir)
-#
-# # --- create blocks ---
-# reservoir.create_blocks(neurons_block, connect_matrix_blocks, dynamics_reservoir, dynamics_synapse,
-#                          dynamics_synapse_pre, threshold='v > 15', reset='v = 13.5', refractory=3 * ms)
-#
-# #--- create synapses between blocks in reservoir---
-# reservoir.create_synapse(connect_matrix_reservoir, dynamics_synapse, dynamics_synapse_pre, blocks_input, blocks_output)
-# reservoir.connect_blocks()
-# net.register_layer(reservoir,'reservoir')
-#
-# #--- create synapses encoding and readout between reservoir---
-# net.create_synapse_encoding(dynamics_synapse, dynamics_synapse_pre, reservoir_input)
-# net.create_synapse_readout(dynamics_synapse_pre)
-# net.connect_encoding()
-# net.connect_readout()
-# net.join_network()
-
-
-# ------------------------------------
-#--- initialize the parameters encoding and readout neurons ---
-# encoding.property = '1'
-# readout.v = '0'
-# readout.g = '0'
-# readout.tau = 30
-#
-# #--- initialize the parameters reservoir and blocks ---
-# reservoir.initialize_blocks_neurons(v = np.array([13.5]*neurons_block) + np.random.rand(neurons_block)*1.5,
-#                                     g = np.array([0] * neurons_block),
-#                                     tau = np.array([tau_neurons] * neurons_block))
-#
-# reservoir.initialize_blocks_synapses(w = strength_synapse_block,
-#                                      delay = delay_synapse_block)
-#
-# reservoir.initialize_reservoir_synapses(w = strength_synapse_reservoir,
-#                                         delay = delay_synapse_reservoir)
-#
-# net.initialize_parameters_encoding_synapses(w = strength_synapse_encoding_reservoir)
 
 
