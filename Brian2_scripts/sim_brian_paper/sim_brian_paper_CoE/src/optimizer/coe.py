before run: coe, precision bug

Brian2_scripts/sim_brian_paper/sim_brian_paper_CoE/src/optimizer/coe.py
@@ -27,6 +27,7 @@ class CoE_surrogate(BaseFunctions):
         self.surrogate = create_surrogate(surrogate_type = surrogate_type , f = f, random_state= random_state,
                                           keys=keys, ranges=ranges, borders=borders, precisions=precisions,
                                           **surrogate_parameters)
+        self._space = self.surrogate._space
 
     def aimfunc(self, Phen, LegV): # for GA with the LegV input and output
         res = []
@@ -262,7 +263,7 @@ class CoE_surrogate_mixgentype(CoE_surrogate):
             SelCh = ga.mutbga(SelCh, FieldDR_i, pm)  # 变异
             if distribute == True and repnum[index] > P_i.shape[0] * 0.01:  # 当最优个体重复率高达1%时，进行一次高斯变异
                 SelCh = ga.mutgau(SelCh, FieldDR_i, pm)  # 高斯变异
-        return SelCh
+        return self._space.add_precision(SelCh,self._space._precisions)
 
     def is2(self, FieldD):
         r = FieldD[2, :] - FieldD[1, :]
@@ -293,6 +294,7 @@ class CoE_surrogate_mixgentype(CoE_surrogate):
             B_i = ga.crtrp(1, FieldDR_i)
             B_i = self.b_coding(B_i, SubCom_i)
             B.extend(list(B_i[0]))
+        B = self._space.add_precision(B, self._space._precisions)
         return np.array(B).reshape(1,-1)
 
     def initialize_offspring(self, NIND, B):
@@ -307,6 +309,7 @@ class CoE_surrogate_mixgentype(CoE_surrogate):
             P_i = self.b_coding(P_i, SubCom_i)
             # 替换context vector中个体基因
             Chrom = B.copy().repeat(NIND, axis=0)
+            Chrom = self._space.add_precision(Chrom,self._space._precisions)
             Chrom[:, SubCom_i] = P_i
             LegV_i = np.ones((NIND, 1))
             # 求子问题的目标函数值
@@ -519,6 +522,7 @@ class CoE_surrogate_mixgentype(CoE_surrogate):
             # 替换context vector中个体基因
             Chrom = B.copy().repeat(NIND, axis=0)
             Chrom[:, SubCom_i] = P_i
+            Chrom = self._space.add_precision(Chrom,self._space._precisions)
             LegV_i = np.ones((NIND, 1))
             # 求子问题的目标函数值
             [ObjV_i, LegV_i] = self.aimfunc(Chrom, LegV_i)

Brian2_scripts/sim_brian_paper/sim_brian_paper_CoE/src/optimizer/surrogate.py
@@ -68,14 +68,12 @@ class TargetSpace(object):
         self.target_func = target_func
 
         # Get the name of the parameters
-        self._keys = sorted(self.pbounds)
-        # Create an array with parameters bounds
-        self._bounds = np.array(
-            [item[1] for item in sorted(self.pbounds.items(), key=lambda x: x[0])],
-            dtype=np.float
-        )
+        self._keys = keys
 
-        self._precisions = [self.pprecisions[x] for x in self._keys]
+        # Create an array with parameters bounds
+        self._bounds = self.FieldDR.T
+
+        self._precisions = precisions
 
         # preallocated memory for X and Y points
         self._params = np.empty(shape=(0, self.dim))
@@ -126,6 +124,14 @@ class TargetSpace(object):
                 bound_[1] = bound_[1] - 1 / (10 ** p)
             bounds[index] = bound_
         return bounds.T
+
+    def add_precision(self, x, precisions):
+        shape = x.shape
+        result = np.zeros(shape)
+        for i in range(shape[1]):
+            if precisions[i] == 0:
+                result[:, i] = np.round(x[:, i], precisions[i])
+        return result
 
     def params_to_array(self, params):
         try:
@@ -368,16 +374,10 @@ class Surrogate():
         """Make sure there's something in the queue at the very beginning."""
         if self._queue.empty and self._space.empty:
             init_points = max(init_points, 1)
-        LHS_points = self.add_precision(self.LHSample(init_points, self._space.bounds), self._space._precisions)
+        LHS_points = self._space.add_precision(self.LHSample(init_points, self._space.bounds),
+                                        self._space._precisions)
         for point in LHS_points:
             self._queue.add(point)
-
-    def add_precision(self, x, precisions):
-        shape = x.shape
-        result = np.zeros(shape)
-        for i in range(shape[1]):
-            result[:, i] = np.round(x[:, i], precisions[i])
-        return result
 
     def LHSample(self, N, bounds, D=None):
         if D == None: