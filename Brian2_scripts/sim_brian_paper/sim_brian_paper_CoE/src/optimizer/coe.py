running: coe precision bug fixed

Brian2_scripts/sim_brian_paper/sim_brian_paper_CoE/src/optimizer/coe.py
@@ -294,8 +294,7 @@ class CoE_surrogate_mixgentype(CoE_surrogate):
             B_i = ga.crtrp(1, FieldDR_i)
             B_i = self.b_coding(B_i, SubCom_i)
             B.extend(list(B_i[0]))
-        B = self._space.add_precision(B, self._space._precisions)
-        return np.array(B).reshape(1,-1)
+        return self._space.add_precision(np.array(B).reshape(1,-1), self._space._precisions)
 
     def initialize_offspring(self, NIND, B):
         P = []

Brian2_scripts/sim_brian_paper/sim_brian_paper_CoE/src/optimizer/surrogate.py
@@ -129,8 +129,7 @@ class TargetSpace(object):
         shape = x.shape
         result = np.zeros(shape)
         for i in range(shape[1]):
-            if precisions[i] == 0:
-                result[:, i] = np.round(x[:, i], precisions[i])
+            result[:, i] = np.round(x[:, i], precisions[i])
         return result
 
     def params_to_array(self, params):