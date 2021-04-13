126cf31cd318c1938ed25a353c847c901ee86195
scarecrow <zhouyanasd@gmail.com>
2021-04-13 20:55:00 +0800

    coe: start reconstruction coe class

---
+++ Brian2_scripts/sim_brian_paper/sim_brian_paper_CoE/src/optimizer/coe_modifed.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+    The optimization methods used for NAS.
+
+:Author: Yan Zhou
+
+:License: BSD 3-Clause, see LICENSE file.
+"""
+from Brian2_scripts.sim_brian_paper.sim_brian_paper_CoE.src.core import BaseFunctions
+from Brian2_scripts.sim_brian_paper.sim_brian_paper_CoE.src.optimizer.surrogate import create_surrogate
+from Brian2_scripts.sim_brian_paper.sim_brian_paper_CoE.src.optimizer import ga as ga
+
+import time, pickle
+
+import numpy as np
+
+
+class CoE(BaseFunctions):
+    pass
+
+
+
+
+
+
+
+
+
+
+class CoE_surrogate(CoE):
+    pass
+
+
+
+
+
+
+
+
+class CoE_surrogate_mixgentype(CoE_surrogate):
+    pass
