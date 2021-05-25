84bef81ad858b2d14af88f2c65d3eb12361eabf9
scarecrow <zhouyanasd@gmail.com>
2021-05-25 20:45:25 +0800

    add ray config for ray_cluster_3

--- Brian2_scripts/sim_brian_paper/sim_brian_paper_CoE/src/ray_config.py
+++ Brian2_scripts/sim_brian_paper/sim_brian_paper_CoE/src/ray_config.py
@@ -49,3 +49,30 @@ ray_cluster_two = {
                           'ray stop'],
     }
 }
+
+
+ray_cluster_three = {
+    'head': {
+        'manage_address': '192.168.101.179',
+        'manage_port': 22,
+        'address': '192.168.101.179',
+        'name': 'zy',
+        'password': '88507840',
+        'dashboard': '8265',
+        'start_commends': ['source Project/DL-NC/venv/bin/activate',
+                          'ray start --head --port=6379 --dashboard-host="0.0.0.0"'],
+        'stop_commends': ['source Project/DL-NC/venv/bin/activate',
+                           'ray stop'],
+    },
+    'nodes':{
+        'manage_address_list': ['192.168.101.180'],
+        'manage_port_list': [22],
+        'address_list':['192.168.101.180'],
+        'name_list':['zy'],
+        'password_list':['88507840'],
+        'start_commends': ['source Project/DL-NC/venv/bin/activate',
+                          'ray start --address="192.168.101.179:6379" --redis-password="5241590000000000"'],
+        'stop_commends': ['source Project/DL-NC/venv/bin/activate',
+                          'ray stop'],
+    }
+}
