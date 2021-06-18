08718ac0af28ec64f7dec2a73f6d328228cc8852
scarecrow <zhouyanasd@gmail.com>
2021-06-18 22:20:50 +0800

    add feature selection according to "Surrogate-Assisted Evolutionary Deep Learning Using an End-to-End Random Forest-based Performance Predictor"

--- Brian2_scripts/sim_brian_paper/sim_brian_paper_CoE/src/optimizer/random_forest.py
+++ Brian2_scripts/sim_brian_paper/sim_brian_paper_CoE/src/optimizer/random_forest.py
@@ -1,11 +1,25 @@
 import threading
+import warnings
+from warnings import warn
 
 import numpy as np
+from scipy.sparse import issparse
 
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.ensemble.base import  _partition_estimators
-from sklearn.utils.validation import check_is_fitted
+from sklearn.ensemble.base import _partition_estimators
+from sklearn.ensemble.forest import _parallel_build_trees
+from sklearn.utils.validation import check_is_fitted, check_array, check_random_state
 from sklearn.externals.joblib import Parallel, delayed
+from sklearn.exceptions import DataConversionWarning
+from sklearn.tree._tree import DTYPE, DOUBLE
+
+MAX_INT = np.iinfo(np.int32).max
+
+def choice_feature(X, n_estimators, c_features):
+    I = []
+    for i in range(n_estimators):
+        I.append(np.random.choice(np.arange(X.shape[1]), size=c_features, replace=False))
+    return I
 
 
 def accumulate_prediction(predict, X, out, out_, lock):
@@ -36,7 +50,8 @@ class RandomForestRegressor_(RandomForestRegressor):
                  n_jobs=1,
                  random_state=None,
                  verbose=0,
-                 warm_start=False):
+                 warm_start=False,
+                 c_features=None):
         super(RandomForestRegressor_, self).__init__(
             n_estimators=n_estimators,
             criterion=criterion,
@@ -54,6 +69,143 @@ class RandomForestRegressor_(RandomForestRegressor):
             random_state=random_state,
             verbose=verbose,
             warm_start=warm_start)
+
+        self.c_features = c_features
+
+    def fit(self, X, y, sample_weight=None):
+        """Build a forest of trees from the training set (X, y).
+
+        Parameters
+        ----------
+        X : array-like or sparse matrix of shape = [n_samples, n_features]
+            The training input samples. Internally, its dtype will be converted
+            to ``dtype=np.float32``. If a sparse matrix is provided, it will be
+            converted into a sparse ``csc_matrix``.
+
+        y : array-like, shape = [n_samples] or [n_samples, n_outputs]
+            The target values (class labels in classification, real numbers in
+            regression).
+
+        sample_weight : array-like, shape = [n_samples] or None
+            Sample weights. If None, then samples are equally weighted. Splits
+            that would create child nodes with net zero or negative weight are
+            ignored while searching for a split in each node. In the case of
+            classification, splits are also ignored if they would result in any
+            single class carrying a negative weight in either child node.
+
+        Returns
+        -------
+        self : object
+        """
+
+        if self.n_estimators == 'warn':
+            warnings.warn("The default value of n_estimators will change from "
+                          "10 in version 0.20 to 100 in 0.22.", FutureWarning)
+            self.n_estimators = 10
+
+        # Validate or convert input data
+        X = check_array(X, accept_sparse="csc", dtype=DTYPE)
+        y = check_array(y, accept_sparse='csc', ensure_2d=False, dtype=None)
+        if sample_weight is not None:
+            sample_weight = check_array(sample_weight, ensure_2d=False)
+        if issparse(X):
+            # Pre-sort indices to avoid that each individual tree of the
+            # ensemble sorts the indices.
+            X.sort_indices()
+
+        # Remap output
+        n_samples, self.n_features_ = X.shape
+
+        if self.c_features != None:
+            self.n_features_ = int(self.c_features)
+
+        y = np.atleast_1d(y)
+        if y.ndim == 2 and y.shape[1] == 1:
+            warn("A column-vector y was passed when a 1d array was"
+                 " expected. Please change the shape of y to "
+                 "(n_samples,), for example using ravel().",
+                 DataConversionWarning, stacklevel=2)
+
+        if y.ndim == 1:
+            # reshape is necessary to preserve the data contiguity against vs
+            # [:, np.newaxis] that does not.
+            y = np.reshape(y, (-1, 1))
+
+        self.n_outputs_ = y.shape[1]
+
+        y, expanded_class_weight = self._validate_y_class_weight(y)
+
+        if getattr(y, "dtype", None) != DOUBLE or not y.flags.contiguous:
+            y = np.ascontiguousarray(y, dtype=DOUBLE)
+
+        if expanded_class_weight is not None:
+            if sample_weight is not None:
+                sample_weight = sample_weight * expanded_class_weight
+            else:
+                sample_weight = expanded_class_weight
+
+        # Check parameters
+        self._validate_estimator()
+
+        if not self.bootstrap and self.oob_score:
+            raise ValueError("Out of bag estimation only available"
+                             " if bootstrap=True")
+
+        random_state = check_random_state(self.random_state)
+
+        if not self.warm_start or not hasattr(self, "estimators_"):
+            # Free allocated memory, if any
+            self.estimators_ = []
+            self.I = []
+
+        n_more_estimators = self.n_estimators - len(self.estimators_)
+
+        if n_more_estimators < 0:
+            raise ValueError('n_estimators=%d must be larger or equal to '
+                             'len(estimators_)=%d when warm_start==True'
+                             % (self.n_estimators, len(self.estimators_)))
+
+        elif n_more_estimators == 0:
+            warn("Warm-start fitting without increasing n_estimators does not "
+                 "fit new trees.")
+        else:
+            if self.warm_start and len(self.estimators_) > 0:
+                # We draw from the random state to get the random state we
+                # would have got if we hadn't used a warm_start.
+                random_state.randint(MAX_INT, size=len(self.estimators_))
+
+            trees = []
+            for i in range(n_more_estimators):
+                tree = self._make_estimator(append=False,
+                                            random_state=random_state)
+                trees.append(tree)
+            I = choice_feature(X, n_more_estimators, self.n_features_)
+            # Parallel loop: we prefer the threading backend as the Cython code
+            # for fitting the trees is internally releasing the Python GIL
+            # making threading more efficient than multiprocessing in
+            # that case. However, we respect any parallel_backend contexts set
+            # at a higher level, since correctness does not rely on using
+            # threads.
+            trees = Parallel(n_jobs=self.n_jobs, verbose=self.verbose,
+                             backend="threading")(
+                delayed(_parallel_build_trees)(
+                    t, self, X[:, I_i], y, sample_weight, i, len(trees),
+                    verbose=self.verbose, class_weight=self.class_weight)
+                for i, (t, I_i) in enumerate(zip(trees, I)))
+
+            # Collect newly grown trees
+            self.estimators_.extend(trees)
+            self.I.extend(I)
+
+        if self.oob_score:
+            self._set_oob_score(X, y)
+
+        # Decapsulate classes_ attributes
+        if hasattr(self, "classes_") and self.n_outputs_ == 1:
+            self.n_classes_ = self.n_classes_[0]
+            self.classes_ = self.classes_[0]
+
+        return self
 
     def predict(self, X):
         """Predict regression target for X.
@@ -75,7 +227,12 @@ class RandomForestRegressor_(RandomForestRegressor):
         """
         check_is_fitted(self, 'estimators_')
         # Check data
-        X = self._validate_X_predict(X)
+        if self.c_features != None:
+            self.estimators_[0].n_features_ = X.shape[1]
+            X = self._validate_X_predict(X)
+            self.estimators_[0].n_features_ = self.c_features
+        else:
+            X = self._validate_X_predict(X)
 
         # Assign chunk of trees to jobs
         n_jobs, _, _ = _partition_estimators(self.n_estimators, self.n_jobs)
@@ -91,8 +248,8 @@ class RandomForestRegressor_(RandomForestRegressor):
         # Parallel loop
         lock = threading.Lock()
         Parallel(n_jobs=n_jobs, verbose=self.verbose, backend="threading")(
-            delayed(accumulate_prediction)(e.predict, X, [y_hat], self.y_hat_, lock)
-            for e in self.estimators_)
+            delayed(accumulate_prediction)(e.predict, X[:, I_i], [y_hat], self.y_hat_, lock)
+            for e, I_i in zip(self.estimators_, self.I))
 
         y_hat /= len(self.estimators_)
 
