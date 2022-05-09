84a11b81454e2663f066c378c2c41207bc34e4a1
scarecrow <zhouyanasd@gmail.com>
2022-05-09 15:03:00 +0800

    Firstly build the codes of MRMT

---
+++ Brian2_scripts/sim_brian_paper/sim_brian_paper_MRMT/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+    Codes for "Bio-inspired Modular Reservoir based on Spiking Neurons for Multi-task Learning"
+
+:Author: Yan Zhou
+
+:License: BSD 3-Clause, see LICENSE file.
+
+Requirement
+=======
+Numpy
+Pandas
+Brian2
+
+Usage
+=======
+
+Citation
+=======
+
+"""
